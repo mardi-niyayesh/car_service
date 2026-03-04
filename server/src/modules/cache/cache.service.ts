802175a5ddd4c1d258fccc4e3c2cca48b5c0a86e
homow <homowsh897@gmail.com>
2026-03-04 06:47:43 +0330

    refactor(service) add keyPrefix to redis.service

--- server/src/modules/cache/cache.service.ts
+++ server/src/modules/cache/cache.service.ts
@@ -3,20 +3,21 @@ import {Cache} from "@nestjs/cache-manager";
 
 @Injectable()
 export class CacheService {
+  private static readonly key: string = process.env.REDIS_KEY_PREFIX || "app";
+
   constructor(private readonly cache: Cache) {}
 
   prefix(key: string): string {
+    if (key.startsWith(CacheService.key)) return key;
     return `${process.env.REDIS_KEY_PREFIX || "app"}:${key}`;
   }
 
   async get<T>(key: string): Promise<T | undefined> {
-    const f = this.cache.stores;
-    console.log(f);
-    return await this.cache.get(key);
+    return await this.cache.get(this.prefix(key));
   }
 
   async set<T>(key: string, value: T, ttl?: number): Promise<T> {
-    return await this.cache.set(key, value, ttl);
+    return await this.cache.set(th, value, ttl);
   }
 
   async del(key: string): Promise<boolean> {
