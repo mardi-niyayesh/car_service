7ebc3220e1d19460011c3d47f8eebd02be85b171
homow <homowsh897@gmail.com>
2026-03-05 18:36:35 +0330

    fix(interceptor) change return cacheValue before map

--- server/src/common/interceptors/cacheable.interceptor.ts
+++ server/src/common/interceptors/cacheable.interceptor.ts
@@ -1,6 +1,6 @@
 import {RedisKey} from "@/lib";
-import {map, Observable} from "rxjs";
 import {Reflector} from "@nestjs/core";
+import {map, Observable, from} from "rxjs";
 import {RedisService} from "@/modules/redis/redis.service";
 import {CACHEABLE_KEY, type CacheableDecoratorType} from "@/common";
 import {CallHandler, ExecutionContext, Injectable, NestInterceptor} from "@nestjs/common";
@@ -12,7 +12,7 @@ export class CacheableInterceptor implements NestInterceptor {
     private readonly redisService: RedisService,
   ) {}
 
-  intercept(ctx: ExecutionContext, next: CallHandler<unknown>): Observable<unknown> | Promise<Observable<unknown>> {
+  async intercept(ctx: ExecutionContext, next: CallHandler<unknown>): Promise<Observable<unknown>> {
     const cacheableKey = this.reflector.getAllAndOverride<CacheableDecoratorType>(CACHEABLE_KEY, [
       ctx.getClass(),
       ctx.getHandler(),
@@ -20,24 +20,24 @@ export class CacheableInterceptor implements NestInterceptor {
 
     if (!cacheableKey) return next.handle();
 
+    // build key pattern
+    const key: string = RedisKey.keyPrefix({
+      ctx,
+      paramsKey: cacheableKey.paramsKey,
+      extraKeys: cacheableKey.extraKeys,
+      resource: cacheableKey.resource,
+      pagination: cacheableKey.pagination,
+      self: cacheableKey.self,
+    });
+
+    // check exist cached
+    const cacheValue = await this.redisService.get(key);
+
+    // exist cached
+    if (cacheValue !== null) return from([cacheValue]);
+
     return next.handle().pipe(
       map(async data => {
-
-        // build key pattern
-        const key: string = RedisKey.keyPrefix({
-          ctx,
-          paramsKey: cacheableKey.paramsKey,
-          extraKeys: cacheableKey.extraKeys,
-          resource: cacheableKey.resource,
-          pagination: cacheableKey.pagination,
-          self: cacheableKey.self,
-        });
-
-        // check exist cached
-        const cacheValue = await this.redisService.get(key);
-
-        // exist cached
-        if (cacheValue !== null) return cacheValue;
 
         // set value with key
         await this.redisService.set(key, data, cacheableKey.ttl);
