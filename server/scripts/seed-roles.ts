fa0dddf5c663ee6e637fd1ccf8fcdae54e343128
homow <homowsh897@gmail.com>
2026-03-05 20:27:56 +0330

    refactor(scripts) add new owner permissions to seed roles

--- server/scripts/seed-roles.ts
+++ server/scripts/seed-roles.ts
@@ -6,12 +6,14 @@ import "tsconfig-paths/register";
 import {NestFactory} from "@nestjs/core";
 import {CliModule} from "@/modules/cli/cli.module";
 import {INestApplicationContext} from "@nestjs/common";
+import {DefaultArgs} from "@prisma/client/runtime/client";
 import {PrismaService} from "@/modules/prisma/prisma.service";
+import {PrismaClient} from "@/modules/prisma/generated/client";
 import {PERMISSIONS, ROLES, RolesType, PermissionsType} from "@/common";
 
 interface SeedCreateRoleParams {
   app: INestApplicationContext;
-  prisma: PrismaService;
+  prisma: Omit<PrismaClient<never, undefined, DefaultArgs>, "$connect" | "$disconnect" | "$on" | "$transaction" | "$extends"> | PrismaService;
   role: RolesType;
   permissions: PermissionsType;
   not?: PermissionsType[];
@@ -24,13 +26,17 @@ async function createNewRole(data: SeedCreateRoleParams): Promise<void> {
   const {app, prisma, role, permissions, not, advanced, description} = data;
 
   if (role === ROLES.OWNER || role === ROLES.SELF) {
-    console.log(`Cannot create a new role with name: ${role}`);
+    console.log(`Cannot create a new role with name: ${role} in ${createNewRole.name}, Cause this route create in ${bootstrap.name}`);
     await app.close();
     process.exit(1);
   }
 
-  const newRole = await prisma.role.create({
-    data: {
+  const newRole = await prisma.role.upsert({
+    where: {
+      name: role,
+    },
+    update: {},
+    create: {
       name: role,
       description
     }
@@ -87,91 +93,127 @@ async function bootstrap(): Promise<void> {
   const prisma = app.get(PrismaService);
 
   const permissions = Object.entries(PERMISSIONS)
-    .filter(p => p[1] !== "user.self")
+    .filter(p => p[1] !== PERMISSIONS.USER_SELF && p[1] !== PERMISSIONS.OWNER_ALL)
     .map(p => p[1]);
 
-  for (const p of permissions) {
-    await prisma.permission.create({
-      data: {
-        name: p,
-        description: `this permission allows ${p.split(".")[1]} on ${p.split(".")[0]}`,
+  await prisma.$transaction(async tx => {
+    for (const p of permissions) {
+      await tx.permission.upsert({
+        where: {
+          name: p
+        },
+        update: {},
+        create: {
+          name: p,
+          description: `this permission allows ${p.split(".")[1]} on ${p.split(".")[0]}`,
+        }
+      });
+    }
+
+    const selfPermission = await tx.permission.upsert({
+      where: {
+        name: PERMISSIONS.USER_SELF
+      },
+      update: {},
+      create: {
+        name: PERMISSIONS.USER_SELF,
+        description: "Basic permission for users to view and update their own personal information"
       }
     });
-  }
 
-  const selfPermission = await prisma.permission.create({
-    data: {
-      name: "user.self",
-      description: "Basic permission for users to view and update their own personal information"
+    const ownerPermission = await tx.permission.upsert({
+      where: {
+        name: PERMISSIONS.OWNER_ALL
+      },
+      update: {},
+      create: {
+        name: PERMISSIONS.OWNER_ALL,
+        description: "this permission allows to access all routes"
+      }
+    });
+
+    const selfRole = await tx.role.upsert({
+      where: {
+        name: ROLES.SELF
+      },
+      update: {},
+      create: {
+        name: ROLES.SELF,
+        description: "Basic role for users to view and update their own personal information",
+      }
+    });
+
+    const ownerRole = await tx.role.upsert({
+      where: {
+        name: ROLES.OWNER
+      },
+      update: {},
+      create: {
+        name: ROLES.OWNER,
+        description: "System owner with full access to all resources"
+      }
+    });
+
+    if (!ownerRole || !selfPermission || !selfRole || !ownerRole) {
+      console.log("⚠ Something went wrong in Server! ownerRole or selfPermission or selfRole or userManagerRole not created");
+      await app.close();
+      process.exit(1);
     }
-  });
 
-  const selfRole = await prisma.role.create({
-    data: {
-      name: ROLES.SELF,
-      description: "Basic role for users to view and update their own personal information",
-    }
-  });
+    await tx.rolePermission.create({
+      data: {
+        role_id: selfRole.id,
+        permission_id: selfPermission.id,
+      }
+    });
 
-  const ownerRole = await prisma.role.create({
-    data: {
-      name: ROLES.OWNER,
-      description: "System owner with full access to all resources"
-    }
-  });
+    await tx.rolePermission.create({
+      data: {
+        role_id: ownerRole.id,
+        permission_id: ownerPermission.id
+      }
+    });
 
-  if (!ownerRole || !selfPermission || !selfRole) {
-    console.log("⚠ Something went wrong in Server! ownerRole or selfPermission or selfRole or userManagerRole not created");
+    await createNewRole({
+      app,
+      prisma: tx,
+      role: ROLES.USER_MANAGER,
+      permissions: PERMISSIONS.USER_VIEW,
+      not: [PERMISSIONS.USER_SELF],
+      advanced: [PERMISSIONS.ROLE_ASSIGN, PERMISSIONS.ROLE_REVOKE],
+      description: "Full administrative access to manage all users in the system",
+    });
+
+    await createNewRole({
+      app,
+      prisma: tx,
+      role: ROLES.ROLE_MANAGER,
+      permissions: PERMISSIONS.ROLE_VIEW,
+      not: [PERMISSIONS.ROLE_ASSIGN, PERMISSIONS.ROLE_REVOKE],
+      description: "Full administrative access to manage all roles in the system",
+    });
+
+    await createNewRole({
+      app,
+      prisma: tx,
+      role: ROLES.CATEGORY_MANAGER,
+      permissions: PERMISSIONS.CATEGORY_CREATE,
+      description: "Full administrative access to manage all categories in the system",
+    });
+
+    await createNewRole({
+      app,
+      prisma: tx,
+      role: ROLES.PRODUCT_MANAGER,
+      permissions: PERMISSIONS.PRODUCT_CREATE,
+      description: "Full administrative access to manage all products in the system",
+    });
+
+    console.log("✅ Seed completed: Default roles and permissions have been created successfully.");
+
     await app.close();
-    process.exit(1);
-  }
-
-  await prisma.rolePermission.create({
-    data: {
-      role_id: selfRole.id,
-      permission_id: selfPermission.id,
-    }
+    process.exit(0);
   });
-
-  await createNewRole({
-    app,
-    prisma,
-    role: ROLES.USER_MANAGER,
-    permissions: PERMISSIONS.USER_VIEW,
-    not: [PERMISSIONS.USER_SELF],
-    advanced: [PERMISSIONS.ROLE_ASSIGN, PERMISSIONS.ROLE_REVOKE],
-    description: "Full administrative access to manage all users in the system",
-  });
-
-  await createNewRole({
-    app,
-    prisma,
-    role: ROLES.ROLE_MANAGER,
-    permissions: PERMISSIONS.ROLE_VIEW,
-    not: [PERMISSIONS.ROLE_ASSIGN, PERMISSIONS.ROLE_REVOKE],
-    description: "Full administrative access to manage all roles in the system",
-  });
-
-  await createNewRole({
-    app,
-    prisma,
-    role: ROLES.CATEGORY_MANAGER,
-    permissions: PERMISSIONS.CATEGORY_CREATE,
-    description: "Full administrative access to manage all categories in the system",
-  });
-
-  await createNewRole({
-    app,
-    prisma,
-    role: ROLES.PRODUCT_MANAGER,
-    permissions: PERMISSIONS.PRODUCT_CREATE,
-    description: "Full administrative access to manage all products in the system",
-  });
-
-  console.log("✅ Seed completed: Default roles and permissions have been created successfully.");
-
-  await app.close();
-  process.exit(0);
 }
 
 bootstrap()
