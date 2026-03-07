d4ec2f68a1b1255adaa42193741c1329c0a47f89
niyayesh <niyayeshammardii@gmail.com>
2026-03-07 04:27:46 -0800

    compleat component DetalisUser for panelAdmin

---
+++ client/src/PanelAdmin/Pages/DetalisUserPage.tsx
@@ -0,0 +1,45 @@
+//components
+import ComponentsPanelAdmin from "../Components/ComponentsPanelAdmin";
+import ComponentCategoryDatailUser from "../Components/ComponentCategoryDatailUser";
+import ComponentFormUpdateUser from "../Components/ComponentFormUpdateUser";
+//icon or logo
+import ArrowLeft from "../../../assets/Arrowleft.png";
+//
+import { Link } from "react-router-dom";
+
+const DetalisUserPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row  md:gap-4 p-3 md:p-4 min-h-screen">
+      {/*Components PanelAdmin */}
+      <div className=" md:block md:w-64 lg:w-72 xl:w-80">
+        <ComponentsPanelAdmin />
+      </div>
+
+      <div className="flex-1">
+        {/* header User*/}
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between  p-3 sm:p-4">
+          <p className="text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
+            جزعیات کاربر
+          </p>
+          <Link to="/panel/users">
+            <img
+              src={ArrowLeft}
+              alt="ArrowLeft"
+              className="w-10 h-10  rounded-4xl p-2  border border-gray-600"
+            />
+          </Link>
+        </div>
+        {/*Components CategoryDatailUser */}
+        <div className="flex flex-col gap-4 ">
+          <ComponentCategoryDatailUser />
+        </div>
+        {/*Components FormUpdateUser */}
+        <div className="flex flex-col gap-4 ">
+          <ComponentFormUpdateUser />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetalisUserPage;
