9fe650fd0dae70e62445caff61d23c12e2adc813
niyayesh <niyayeshammardii@gmail.com>
2026-03-07 06:40:30 -0800

    complear ui component product for panel Admin

---
+++ client/src/PanelAdmin/Pages/ProductPage.tsx
@@ -0,0 +1,37 @@
+//components
+import ComponentCategoryProduct from "../Components/ComponentCategoryProduct";
+import ComponentsPanelAdmin from "../Components/ComponentsPanelAdmin";
+import ComponentTableProduct from "../Components/ComponentTableProduct";
+import ComponentFormAddProduct from "../Components/ComponentFormAddProduct";
+const ProductPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row  md:gap-4 p-3 md:p-4 min-h-screen">
+      {/*Components PanelAdmin */}
+      <div className=" md:block md:w-64 lg:w-72 xl:w-80">
+        <ComponentsPanelAdmin />
+      </div>
+      <div className="flex-1">
+        {/* header product*/}
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between  p-3 sm:p-4">
+          <p className="text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
+            محصولات
+          </p>
+        </div>
+        {/*Components CategoryProduct  */}
+        <div className="flex flex-col gap-4 ">
+          <ComponentCategoryProduct />
+        </div>
+        {/*Components TableProduct */}
+        <div dir="rtl" className="p-4">
+          <ComponentTableProduct />
+        </div>
+          {/*Components FormAddProduct */}
+        <div className="flex flex-col gap-4 ">
+          <ComponentFormAddProduct />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
