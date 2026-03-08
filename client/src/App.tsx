78cecde1da0b8994c7cb68c3047ae5a16e591b2a
niyayesh <niyayeshammardii@gmail.com>
2026-03-08 01:37:15 -0800

    add pages panelAdmin to Route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
+//components
 import { Routes, Route } from "react-router-dom";
 import HeaderSite from "./components/Header/HeaderSite";
 import Footer from "./components/Footer/Footer";
+import ComponentsPanelAdmin from "./PanelAdmin/Components/ComponentsPanelAdmin";
+import DashboardLayout from "./dashboard/Components/DashboardLayout";
 
 //pages
 import AboutPage from "./pages/AboutPage";
@@ -13,14 +16,18 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
-import DashboardLayout from "./dashboard/Components/DashboardLayout";
+//pages for panelUser
 import AddressPages from "./dashboard/Pages/AddressPages";
 import CommentPages from "./dashboard/Pages/CommentPages";
 import ReservePages from "./dashboard/Pages/ReservePages";
 import WalletPages from "./dashboard/Pages/WalletPages";
 import CardPages from "./dashboard/Pages/CardPages";
 import LogoutPage from "./dashboard/Pages/LogoutPage";
-
+//pages for panelAdmin
+import UsersPage from "./PanelAdmin/Pages/UsersPage";
+import DetalisUserPage from "./PanelAdmin/Pages/DetalisUserPage";
+import ProductPage from "./PanelAdmin/Pages/ProductPage";
+import CategoryPage from "./PanelAdmin/Pages/CategoryPage";
 function App() {
   return (
     <>
@@ -36,6 +43,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
+        {/* Route for panelUser */}
         <Route path="/dashboard" element={<DashboardLayout />} />
         <Route path="/dashboard/address" element={<AddressPages />} />
         <Route path="/dashboard/comment" element={<CommentPages />} />
@@ -43,6 +51,13 @@ function App() {
         <Route path="/dashboard/wallet" element={<WalletPages />} />
         <Route path="/dashboard/card" element={<CardPages />} />
         <Route path="/dashboard/logout" element={<LogoutPage />} />
+        {/* Route for panelAdmin */}
+        <Route path="/panel" element={<ComponentsPanelAdmin />} />
+        <Route path="/panel/users" element={<UsersPage />} />
+        <Route path="/panel/users/detail" element={<DetalisUserPage />} />
+         <Route path="/panel/product" element={<ProductPage />} />
+         <Route path="/panel/category" element={<CategoryPage />} />
+
       </Routes>
       <Footer />
     </>
