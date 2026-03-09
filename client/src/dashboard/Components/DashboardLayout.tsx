ed83de38930993a80124dcc152e17c2b90724486
niyayesh <niyayeshammardii@gmail.com>
2026-03-09 05:33:27 -0700

    cut component DashboardSidebar andr forward DashboardSidebar to DashboardLayout

--- client/src/dashboard/Components/DashboardLayout.tsx
+++ client/src/dashboard/Components/DashboardLayout.tsx
@@ -1,13 +1,18 @@
 import DashboardSidebar from "./DashboardSidebar";
-
+import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
   return (
     <>
-    <div>
-       <DashboardSidebar/>
-    </div>
+      <div className="flex flex-col md:flex-row gap-3 md:gap-4 p-3 md:p-4 min-h-screen bg-white">
+        <div >
+          <DashboardSidebar />
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default DashboardLayout;
