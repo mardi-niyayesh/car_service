9e33de62f42dbbfa5ba33864cb746697b7fb2410
niyayesh <niyayeshammardii@gmail.com>
2026-03-10 12:46:21 -0700

    refactor

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -4,8 +4,6 @@ import { UserContext } from "../Context/UserContext";
 export const useUser = () => {
   const context = useContext(UserContext);
   if (context === undefined || context === null) {
-    // اگر context مقدار null یا undefined داشت، یعنی خارج از Provider استفاده شده.
-    // در اینجا یک خطا پرتاب می‌کنیم تا مشکل مشخص شود.
     throw new Error("کامپوننت ما  خارج از یوزپروایدر قرار گرفته یا مقدار ندارد");
   }
   return context; 
