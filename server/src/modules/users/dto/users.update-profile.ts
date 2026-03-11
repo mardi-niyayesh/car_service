8cc6782ba6ec299a83b8ce7542c99e2361d9010c
homow <homowsh897@gmail.com>
2026-03-11 20:58:30 +0330

    refactor(validator) make optional data

--- server/src/modules/users/dto/users.update-profile.ts
+++ server/src/modules/users/dto/users.update-profile.ts
@@ -5,6 +5,9 @@ import {BaseUserSchema} from "./users.validators";
 export const UpdateProfileValidator = BaseUserSchema.pick({
   age: true,
   display_name: true
+}).partial().refine(data => data.age || data.display_name, {
+  error: 'Either age or display_name must be provided',
+  path: ['age', 'display_name'],
 });
 
 export type UpdateProfileType = z.infer<typeof UpdateProfileValidator>;
