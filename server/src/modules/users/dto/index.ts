bf0c4a2be8bdaeac22bacccfa63e79e9c74505fe
homow <homowsh897@gmail.com>
2026-03-11 20:25:27 +0330

    feat(validator) add a new for update profile

--- server/src/modules/users/dto/index.ts
+++ server/src/modules/users/dto/index.ts
@@ -1,4 +1,5 @@
 export * from "./users.get";
-export * from "./user.role-assigned";
+export * from "./users.role-assigned";
 export * from "./users.role-revoked";
 export * from "./users.get-one";
+export * from "./users.update-profile";
---
+++ server/src/modules/users/dto/users.update-profile.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+import {BaseUserSchema} from "./users.validators";
+
+export const UpdateProfileValidator = BaseUserSchema.pick({
+  age: true,
+  display_name: true
+});
+
+export type UpdateProfileType = z.infer<typeof UpdateProfileValidator>;
