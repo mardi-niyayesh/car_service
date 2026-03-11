f1ff41dfd42a892656d1358adb1b926e734a9426
homow <homowsh897@gmail.com>
2026-03-11 16:48:01 +0330

    refactor(interceptor) change evict-cache structure

--- server/src/common/decorators/cache-evict.decorator.ts
+++ server/src/common/decorators/cache-evict.decorator.ts
@@ -3,23 +3,29 @@ import {type ParamCacheKeyType} from "@/lib";
 
 export const CACHE_EVICT_KEY = "CACHE_EVICT_KEY";
 
-type CacheEvictDecoratorBaseType = Omit<ParamCacheKeyType, "ctx"> & {
+type CacheEvictDecoratorForce = Omit<ParamCacheKeyType, "ctx"> & {
   force?: boolean;
+  prefix?: never;
+};
+
+type CacheEvictDecoratorPrefix = Omit<ParamCacheKeyType, "resource" | 'ctx'> & {
+  force?: never;
+  prefix?: string;
 }
 
-export type CacheEvictDecoratorType = CacheEvictDecoratorBaseType[];
+export type CacheEvictDecorator = CacheEvictDecoratorPrefix | CacheEvictDecoratorForce;
 
 /** Build Key Cache for Delete
  * @example
- - @CacheEvict([{
+ - @CacheEvict({
  self: false,
  paramKey: "id",
  resource: "users"
- }])
+ })
 
  * @return
  * ["app:users:id"]
  * */
-export function CacheEvict(params: CacheEvictDecoratorType) {
+export function CacheEvict(params: CacheEvictDecorator) {
   return SetMetadata(CACHE_EVICT_KEY, params);
 }
