77431471dd9ece577793ff5d436f1dc49cf6683e
niyayesh <niyayeshammardii@gmail.com>
2026-03-11 12:00:13 -0700

    cut UserContext to another file

--- client/src/Context/UserContext.tsx
+++ client/src/Context/UserContext.tsx
@@ -1,9 +1,9 @@
-import { createContext, useState } from "react";
-import {type ReactNode } from "react";
+//hooks
+import { createContext } from "react";
 //types
 import { type User } from "../types/auth.types";
 //creat context
-export const UserContext = createContext<{
+const UserContext = createContext<{
   user: User | null;
   token: string | null;
   setUser: (userData: User | null) => void;
@@ -11,35 +11,4 @@ export const UserContext = createContext<{
   logout: () => void;
 } | null>(null);
 
-// a component to access to UserContext
-export const UserProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUserState] = useState<User | null>(null);
-  const [token, setTokenState] = useState(
-    localStorage.getItem("token") || null,
-  );
-  // update information user
-  const setUser = (userData: User | null) => {
-    setUserState(userData);
-  };
-  // update token user
-  const setToken = (newToken: string | null) => {
-    setTokenState(newToken);
-    if (newToken) {
-      localStorage.setItem("token", newToken);
-    } else {
-      localStorage.removeItem("token");
-    }
-  };
-
-  const logout = () => {
-    setUserState(null);
-    setTokenState(null);
-    localStorage.removeItem("token");
-  };
-
-  return (
-    <UserContext.Provider value={{ user, token, setUser, setToken, logout }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+export default UserContext;
