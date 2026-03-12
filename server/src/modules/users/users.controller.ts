eb094926e075062d25c7648d49b36879f2774a0c
homow <homowsh897@gmail.com>
2026-03-12 19:49:26 +0330

    refactor(controller) add password validator to endpoint

--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -133,7 +133,9 @@ export class UsersController {
   @Patch("password")
   updatePassword(
     @Req() req: AccessRequest,
+    @Body(new ZodPipe(UserDto.UpdatePasswordValidator)) data: UserDto.UpdatePasswordType
   ) {
+    console.log(data);
     return this.usersService.updatePassword(req.user.userId);
   }
 
