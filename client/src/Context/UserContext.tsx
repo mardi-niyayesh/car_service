47c5b112fbea2a8bf1392255f72430a968bea315
niyayesh <niyayeshammardii@gmail.com>
2026-03-13 11:27:00 -0700

    cut type UserContext

--- client/src/Context/UserContext.tsx
+++ client/src/Context/UserContext.tsx
@@ -1,14 +1,8 @@
 //hooks
 import { createContext } from "react";
 //types
-import { type User } from "../types/auth.types";
-//creat context
-const UserContext = createContext<{
-  user: User | null;
-  token: string | null;
-  setUser: (userData: User | null) => void;
-  setToken: (newToken: string | null) => void;
-  logout: () => void;
-} | null>(null);
+import { type UserContextType } from "../types/auth.types";
+
+const UserContext = createContext<UserContextType | null>(null);
 
 export default UserContext;
