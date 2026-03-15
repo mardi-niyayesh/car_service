f2d1614e47d8115c4c5e853b80e050d3f5414f6f
niyayesh <niyayeshammardii@gmail.com>
2026-03-15 10:38:46 -0700

    deleat baseURL

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -3,15 +3,13 @@ import type { AxiosResponse } from "axios";
 //types
 import type { RefreshResponse } from "../types/auth.types";
 
-// const API_BASE_URL = "http://localhost:3000/api";
-
   export async function refreshAuth(): Promise<RefreshResponse | null> {
   try {
     const response: AxiosResponse<RefreshResponse> = await axios.post(
       `api/auth/refresh`,
       {},
     );    
-    console.log(response.data)
+    console.log(" refresh ",response.data)
    return response.data;
   } catch (error) {
     console.error("خطا در رفرش توکن و دریافت اطلاعات کاربر:", error);
