7863e69e06d511d2d79614c3a5abb404479a9b86
niyayesh <niyayeshammardii@gmail.com>
2026-03-16 11:42:01 -0700

    creat component Heade in dashbord

---
+++ client/src/dashboard/Components/HeaderDashbord.tsx
@@ -0,0 +1,51 @@
+//img
+import profilUser from "../../../assets/Ellipse 114.png";
+import logout from "../../../assets/logout.png";
+//custom hook for use context
+import { useUser } from "../../hooks/useUser";
+//hook
+import { useState } from "react";
+//icon
+import { HiSun, HiMoon } from "react-icons/hi";
+
+const HeaderDashbord = () => {
+  //   const { them, setThem } = useState<boolean>(true);
+  const { user } = useUser();
+  const ClickHandler = () => {
+    setThem(!true);
+  };
+  return (
+    <div>
+      <header
+        className="flex justify-between items-center w-full px-6 py-4 bg-white border-b border-gray-300 sticky top-0 z-40" // sticky top-0
+      >
+        <div className="flex items-center gap-3">
+          <img
+            src={profilUser}
+            alt="profilUser"
+            className="w-12 h-12 rounded-full"
+          />
+          <div className="flex-1">
+            <p className="font-bold text-[16px] text-[#353535]  max-w-[200px]">
+              {user?.display_name || "کاربر عزیز"}
+            </p>
+            <p className="text-[#727272] font-medium text-[13px]  max-w-[200px]">
+              {user?.email || "example@email.com"}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between gap-5">
+          <img
+            src={logout}
+            alt="logout"
+            className="w-5 h-5 md:w-6 md:h-6 group-hover:brightness-0 group-hover:invert transition-all duration-300"
+          />
+          {/* <HiSun style={{ opacity: 0.5, cursor: "pointer" }} onClick={ClickHandler} size={24} /> */}
+          <HiMoon style={{ opacity: 0.5, cursor: "pointer" }} size={24} />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderDashbord;
