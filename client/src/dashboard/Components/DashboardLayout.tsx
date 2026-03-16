fd02f20758cd50482a18bcdc2d80d775f2b6e22d
niyayesh <niyayeshammardii@gmail.com>
2026-03-16 11:30:32 -0700

    add HeaderDashbord to component

--- client/src/dashboard/Components/DashboardLayout.tsx
+++ client/src/dashboard/Components/DashboardLayout.tsx
@@ -1,17 +1,20 @@
-import DashboardSidebar from "./DashboardSidebar";
 import { Outlet } from "react-router-dom";
+//components
+import DashboardSidebar from "./DashboardSidebar";
+import HeaderDashbord from "./HeaderDashbord";
 const DashboardLayout = () => {
   return (
-    <>
-      <div className="flex flex-col md:flex-row gap-3 md:gap-4 p-3 md:p-4 min-h-screen bg-white">
-        <div >
-          <DashboardSidebar />
-          <div>
-            <Outlet />
-          </div>
-        </div>
+    <div className="flex bg-[#F6F6F6] min-h-screen">
+      <div className=" md:flex fixed top-0 right-0 h-full w-72 z-40">
+        <DashboardSidebar />
       </div>
-    </>
+      <div className="flex-1 md:mr-72 flex flex-col">
+        <HeaderDashbord />
+        <main className="flex-1 p-4 pb-20 md:pb-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
 
