47f260865b5cb8e5ea4db9c38c937f62e47fab3e
homow <homowsh897@gmail.com>
2026-03-17 17:32:06 +0330

    feat(comment)

--- server/src/modules/roles/dto/roles.get-all.ts
+++ server/src/modules/roles/dto/roles.get-all.ts
@@ -2,6 +2,7 @@ import {RoleResponse} from "@/types";
 import {findOneRoleResponse} from "./roles.get";
 import {getBaseOkResponseSchema} from "@/common";
 
+/** ok example for find all roles */
 export class FindAllRolesOkRes extends getBaseOkResponseSchema<{ roles: RoleResponse[] }>({
   path: "roles?orderBy=desc&limit=5&page=1",
   response: {
