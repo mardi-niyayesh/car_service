7edc3389acf58339ac7a3d90d4045eb1c2b231b0
niyayesh <niyayeshammardii@gmail.com>
2026-03-17 04:41:56 -0700

    refactor and add Header to paneladmin

--- client/src/PanelAdmin/Components/PanelAdminLayout.tsx
+++ client/src/PanelAdmin/Components/PanelAdminLayout.tsx
@@ -1,16 +1,20 @@
 import { Outlet } from "react-router-dom";
 import ComponentsPanelAdmin from "./ComponentsPanelAdmin";
+import HeaderDashbord from "../../dashboard/Components/HeaderDashbord";
 
 const PanelAdminLayout = () => {
   return (
-    <>
-      <div className=" md:block md:w-64 lg:w-72 xl:w-80">
+    <div className="flex bg-[#F6F6F6] min-h-screen">
+      <div className=" md:flex fixed top-0 right-0 h-full w-72 z-40">
         <ComponentsPanelAdmin />
-        <div>
-          <Outlet />
-        </div>
       </div>
-    </>
+      <div className="flex-1 md:mr-72 flex flex-col">
+        <HeaderDashbord />
+        <main className="flex-1 p-4 pb-20 md:pb-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
 
