6e2c9a6277b51177d79a5868ac9177c005387a94
niyayesh <niyayeshammardii@gmail.com>
2026-03-17 12:35:02 -0700

    creat component Header and refactor and fix for scroll

--- client/src/components/Header/HeaderSite.tsx
+++ client/src/components/Header/HeaderSite.tsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react";
+import ContainerHeader from "./ContainerHeader";
+import MenuHeader from "./Components/MenuHeader";
 
-const HeaderSite = () => {
+function HeaderSite() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = 100;
+      if (window.scrollY > scrollThreshold) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const headerClasses = `
+    ${isScrolled ? "fixed top-0 left-0 right-0 z-50 w-full" : "relative"}
+    bg-white shadow-md 
+  `;
+
   return (
- <div>
-  
- </div>
+    <div className="relative  bg-gray-100">
+      <header className={headerClasses}>
+        <ContainerHeader />
+        <MenuHeader />
+      </header>
+    </div>
   );
-};
+}
 
 export default HeaderSite;
