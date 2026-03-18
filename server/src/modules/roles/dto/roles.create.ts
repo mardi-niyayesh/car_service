2d18f53bf91829e85592f3d890ac07e8d589e5b3
homow <homowsh897@gmail.com>
2026-03-18 21:04:57 +0330

    feat(dro) add a new example swagger body for create roles

--- server/src/modules/roles/dto/roles.create.ts
+++ server/src/modules/roles/dto/roles.create.ts
@@ -1,5 +1,7 @@
 import z from "zod";
+import {createZodDto} from "nestjs-zod";
 
+/** validator */
 export const CreateRoleValidator = z.object({
   permissions: z.array(z.uuidv4({error: "Invalid permission ID format"}))
     .nonempty()
@@ -9,4 +11,8 @@ export const CreateRoleValidator = z.object({
     .min(2),
 });
 
+/** Type of validator */
 export type CreateRoleType = z.infer<typeof CreateRoleValidator>;
+
+/** example body for swagger */
+export class CreateRoleDto extends createZodDto(CreateRoleValidator) {}
