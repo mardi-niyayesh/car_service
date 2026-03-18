61273027be72aa9777494849bfc7a000f4124852
niyayesh <niyayeshammardii@gmail.com>
2026-03-18 09:07:16 -0700

    responsive in size mobile

--- client/src/components/Header/Components/Basket.tsx
+++ client/src/components/Header/Components/Basket.tsx
@@ -1,17 +1,21 @@
 import { MdOutlineShoppingCart } from "react-icons/md";
+
 const Basket = () => {
   return (
     <div
-      className="inline-flex items-center justify-between border-2 border-blue-600 gap-2 px-4 py-2 w-auto rounded-lg 
-                   bg-white hover:bg-[#137cfd]  transition duration-300 ease-in-out 
-                   group"
+      className="inline-flex cursor-pointer items-center justify-between  py-2 w-auto rounded-lg
+                   bg-white md:hover:bg-[#137cfd] transition duration-300 ease-in-out
+                   group
+                   sm:justify-center sm:px-2 
+                   md:border-2 md:border-blue-600" 
     >
       <MdOutlineShoppingCart
         size={30}
-        className="text-blue-600 group-hover:text-white transition duration-300 ease-in-out"
+        className="text-blue-600 md:group-hover:text-white transition duration-300 ease-in-out
+                   sm:size={24}"
       />
 
-      <button className="text-blue-600 group-hover:text-white transition duration-300 ease-in-out">
+      <button className="text-blue-600 hidden md:block md:px-1  font-medium group-hover:text-white transition duration-300 ease-in-out">
         سبد خرید
       </button>
     </div>
