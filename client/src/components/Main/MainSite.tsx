22c21ac82530b1bcdca23f11b344451dce3dcb42
niyayesh <niyayeshammardii@gmail.com>
2026-03-19 04:16:06 -0700

    add component WhyAutoRent

--- client/src/components/Main/MainSite.tsx
+++ client/src/components/Main/MainSite.tsx
@@ -1,6 +1,6 @@
 //components
 import CarParts from "./CarParts";
-import WhyAutoRent from "./WhyAutoRent/WhyAutoRent";
+import WhyAutoRent from "./WhyAutoRent";
 import Slider from "./Slider";
 import ReserveAutorent from "./ReserveAutorent";
 import FeacherBoxcommentUser from "../comment/FeacherBoxcommentUser";
