e1b069eca28c0816630ea42039157eddac6664b1
niyayesh <niyayeshammardii@gmail.com>
2026-03-21 09:41:04 -0700

    add img pages qeshm

--- client/src/components/Header/Components/MenuHeader.tsx
+++ client/src/components/Header/Components/MenuHeader.tsx
@@ -1,9 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import {
   FaPhone,
   FaInfoCircle,
   FaBlog,
-  FaRegCalendarAlt,
   FaHandsHelping,
   FaHome,
 } from "react-icons/fa";
@@ -12,8 +12,14 @@ const menuItems = [
   { label: "خانه ", path: "/", icon: <FaHome size={20} /> },
   {
     label: "رزرو خودرو",
-    path: "/car-booking",
-    icon: <FaRegCalendarAlt size={20} />,
+    dropdownItems: [
+      { label: "انتخاب شهر...", value: "", disabled: true },
+      { label: "رزرو خودرو در مشهد", path: "/car-booking/mashhad" },
+      { label: "رزرو خودرو در تهران", path: "/car-booking/tehran" },
+      { label: "رزرو خودرو در اصفهان", path: "/car-booking/isfahan" },
+      { label: "رزرو خودرو در ساری", path: "/car-booking/isfahan" },
+      { label: "رزرو خودرو در قشم", path: "/car-booking/isfahan" },
+    ],
   },
   { label: "خدمات ما", path: "/services", icon: <FaHandsHelping size={20} /> },
   { label: "بلاگ", path: "/blog", icon: <FaBlog size={20} /> },
@@ -22,21 +28,63 @@ const menuItems = [
 ];
 
 const MenuHeader = () => {
+  const navigate = useNavigate();
+
+  const handleSelectChange = (event) => {
+    const selectedPath = event.target.value;
+    if (selectedPath) {
+      navigate(selectedPath);
+    }
+  };
   return (
     <>
       <div className="bg-white py-4 ">
         <nav className=" hidden md:flex container mx-auto px-4 flexed items-center justify-between">
           <ul className="flex items-center space-x-6 ">
-            {menuItems.map((item) => (
-              <li className="text-gray-500 flex items-center">
-                <Link
-                  to={item.path}
-                  className="text-gray-700 hover:text-blue-600 font-medium transition duration-300 ease-in-out px-3 py-2"
-                >
-                  {item.label}
-                </Link>
-              </li>
-            ))}
+            {menuItems.map((item) => {
+              if (item.dropdownItems) {
+                return (
+                  <li
+                    key={item.label}
+                    className="relative  flex items-center group"
+                  >
+                    <div className="flex items-center px-3 py-2 text-gray-700 ">
+                      <span className="font-medium">{item.label}</span>
+                    </div>
+
+                    <select
+                      className="absolute inset-0 w-full h-full opacity-0 p- cursor-pointer bg-transparent appearance-none"
+                      onChange={handleSelectChange}
+                      value=""
+                    >
+                      {item.dropdownItems.map((optionItem) => (
+                        <option
+                          key={optionItem.label}
+                          value={optionItem.path}
+                          disabled={optionItem.disabled || false}
+                        >
+                          {optionItem.label}
+                        </option>
+                      ))}
+                    </select>
+                  </li>
+                );
+              } else {
+                return (
+                  <li
+                    key={item.path}
+                    className="text-gray-500 flex items-center"
+                  >
+                    <Link
+                      to={item.path}
+                      className="text-gray-700 hover:text-blue-600 font-medium transition duration-300 ease-in-out px-3 py-2"
+                    >
+                      {item.label}
+                    </Link>
+                  </li>
+                );
+              }
+            })}
           </ul>
         </nav>
       </div>
