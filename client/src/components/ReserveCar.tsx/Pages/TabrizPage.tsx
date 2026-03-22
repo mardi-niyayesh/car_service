998b1b6fe975c4207dba01cbd45e95c11825d4c9
niyayesh <niyayeshammardii@gmail.com>
2026-03-22 12:26:31 -0700

    creat pages TabrizPage

--- client/src/components/ReserveCar.tsx/Pages/TabrizPage.tsx
+++ client/src/components/ReserveCar.tsx/Pages/TabrizPage.tsx
@@ -1,8 +1,45 @@
 //components
 import DesCar from "../DesCar";
 import ReserveComponent from "../ReserveComponent";
+import CarComponent from "../CarComponent";
 //img tabriz
 import Tabrizimg from "../../../../assets/TABRIZ-2.png";
+
+import car1 from "../../../../assets/206.png";
+import sari3 from "../../../../assets/sari3.png"
+import car3 from "../../../../assets/rana.png";
+import car4 from "../../../../assets/PERSIA.png";
+
+const ItemCar = [
+  {
+    id:2,
+    img: car1,
+    ttile: "پژو ۲۰۶ ",
+    price: "۲٫۴۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+ {
+    id:3,
+    img: sari3,
+    ttile: " ﻫﺎﯾﻤﺎ S8 TURBO",
+    price: "۷٫۱۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+  {
+    id:6,
+    img: car4,
+    ttile: " پژو پرﺷﯿﺎ TU5",
+    price: "۲٫۸۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+  {
+    id:5,
+    img: car3,
+    ttile: "  راﻧﺎ پلاس",
+    price: "۲٫۴۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+];
 
 const TabrizPage = () => {
   return (
@@ -16,6 +53,20 @@ const TabrizPage = () => {
  امروزه به دلیل شرایط اقتصادی حاکم بر کشور بیشتر مردم قدرت خرید خودرو ندارند و برای تفریح و مسافرت به دنبال خودروهای اجاره‌ای هستند. اجاره خودرو در تبریز یک امر رایج و مرسوم است، وسعت زیاد این شهر باعث شده تا نتوانید با وسایل نقلیه عمومی به راحتی تردد کنید. داشتن یک خودروی شخصی به شما کمک می‌کند
   تا در کوتاه‌ترین زمان ممکن و بدون خستگی به مقصد خود برسید."
       />
+      <div className="container mx-auto px-4 py-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {ItemCar.map((car) => (
+            <CarComponent
+              key={car.id}
+              id={car.id}
+              img={car.img}
+              title={car.ttile}
+              price={car.price}
+              duration={car.duration}
+            />
+          ))}
+        </div>
+      </div>
       <DesCar />
     </div>
   );
