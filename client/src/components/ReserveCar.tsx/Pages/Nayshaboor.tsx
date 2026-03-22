5caa03e4f2dcac15184a5c9d0d695a65a61f14e2
niyayesh <niyayeshammardii@gmail.com>
2026-03-22 12:24:46 -0700

    creat pages Nayshabbor

--- client/src/components/ReserveCar.tsx/Pages/Nayshaboor.tsx
+++ client/src/components/ReserveCar.tsx/Pages/Nayshaboor.tsx
@@ -1,8 +1,44 @@
 //components
 import ReserveComponent from "../ReserveComponent";
 import DesCar from "../DesCar";
+import CarComponent from "../CarComponent";
 //img neyshaboor
 import Nayshabbor from "../../../../assets/neydhaboor.png";
+import car1 from "../../../../assets/206.png";
+import car2 from "../../../../assets/sari3.png";
+import car3 from "../../../../assets/rana.png";
+import car4 from "../../../../assets/PERSIA.png";
+
+const ItemCar = [
+  {
+    id:2,
+    img: car1,
+    ttile: "پژو ۲۰۶ ",
+    price: "۲٫۴۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+  {
+    id:3,
+    img:car2,
+    ttile: " ﻫﺎﯾﻤﺎ S8 TURBO",
+    price: "۷٫۱۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+  {
+    id:6,
+    img: car4,
+    ttile: " پژو پرﺷﯿﺎ TU5",
+    price: "۲٫۸۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+  {
+    id:5,
+    img: car3,
+    ttile: "  راﻧﺎ پلاس",
+    price: "۲٫۴۰۰٫۰۰۰",
+    duration: "3 تا 6 روز",
+  },
+];
 
 const Nayshaboor = () => {
   return (
@@ -18,6 +54,20 @@ const Nayshaboor = () => {
 علاوه بر سفر با قطار، می‌توانید با اجاره یک خودرو و به منظور کار یا تفریح به نیشابور سفر کنید. سوییچ رنت یکی از شرکت‌هایی است که خودروهای لوکس و داخلی را با بهترین قیمت در اختیارتان قرار می‌دهد، اگر می‌خواهید با شرایط و نحوه اجاره خودرو در نیشابور بیشتر آشنا شوید
  تا انتهای مقاله با ما همراه باشید. "
       />
+      <div className="container mx-auto px-4 py-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {ItemCar.map((car) => (
+            <CarComponent
+              key={car.id}
+               id={car.id}
+              img={car.img}
+              title={car.ttile}
+              price={car.price}
+              duration={car.duration}
+            />
+          ))}
+        </div>
+      </div>
       <DesCar />
     </div>
   );
