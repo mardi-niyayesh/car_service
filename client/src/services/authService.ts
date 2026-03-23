469ddae3d9e54e07418b330c0347223eb0765648
niyayesh <niyayeshammardii@gmail.com>
2026-03-23 09:29:52 -0700

    refactor

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import type { AxiosResponse } from "axios";
+
 //types
 import type { RefreshResponse } from "../types/auth.types";
 
-  export async function refreshAuth(): Promise<RefreshResponse | null> {
+export async function refreshAuth(): Promise<RefreshResponse | null> {
   try {
     const response: AxiosResponse<RefreshResponse> = await axios.post(
       `api/auth/refresh`,
       {},
-    );    
-    // console.log(" refresh ",response.data)
-   return response.data;
+    );
+    console.log(" refresh ", response.data);
+    return response.data;
   } catch (error) {
     console.error("خطا در رفرش توکن و دریافت اطلاعات کاربر:", error);
+
     return null;
   }
 }
