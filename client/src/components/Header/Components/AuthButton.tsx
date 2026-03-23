2a51e52a9b4a31fcdbc6a304121ddb42358323b6
niyayesh <niyayeshammardii@gmail.com>
2026-03-23 09:27:59 -0700

    add Link dashbord to button Autorization

--- client/src/components/Header/Components/AuthButton.tsx
+++ client/src/components/Header/Components/AuthButton.tsx
@@ -1,15 +1,21 @@
+// AuthButton.tsx (بدون تغییر)
+
+import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
+
 const AuthButton = () => {
   return (
     <div
-      className="inline-flex cursor-pointer items-center justify-between  py-3 w-auto rounded-lg
+      className="inline-flex cursor-pointer items-center justify-between py-3 w-auto rounded-lg
                     md:bg-[#137cfd] transition duration-300 ease-in-out text-white
                    group
-                   sm:justify-center sm:px-2 
+                   sm:justify-center sm:px-2
                    md:hover:bg-[#105ab4]"
     >
-      <FaUser size={20} className="md:block text-blue-900" />
-      <button className="hidden md:block font-medium ">ورود | ثبت نام</button>
+      <Link to="/login" className="flex items-center px-3"> {/* اضافه کردن padding افقی */}
+        <FaUser size={20} className="md:block text-blue-900 mr-2" /> {/* اضافه کردن mr-2 */}
+        <button className="hidden md:block font-medium ">ورود | ثبت نام</button>
+      </Link>
     </div>
   );
 };
