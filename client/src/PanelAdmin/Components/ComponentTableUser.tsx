8e7e53b6351a02b6b57b7dea895c8e1751d3f5cd
niyayesh <niyayeshammardii@gmail.com>
2026-03-24 06:55:50 -0700

    refactor url to detail

--- client/src/PanelAdmin/Components/ComponentTableUser.tsx
+++ client/src/PanelAdmin/Components/ComponentTableUser.tsx
@@ -1,30 +1,30 @@
 const coulmn = [
   {
-    to: "users/detail",
+    to: "detail",
     number: "1",
     nameUser: "niya",
     EmailUser: "niya@gmail.com",
-    role:"کاربر",
+    role: "کاربر",
     more: " بیش تر...",
   },
   {
-    to: "users/detail",
+    to: "detail",
     number: "2",
     nameUser: "niya",
     EmailUser: "niya@gmail.com",
-     role:"کاربر",
+    role: "کاربر",
     more: " بیش تر...",
   },
   {
-    to: "users/detail",
+    to: "detail",
     number: "3",
     nameUser: "niya",
     EmailUser: "niya@gmail.com",
-     role:"کاربر",
+    role: "کاربر",
     more: " بیش تر...",
   },
   {
-    to: "users/detail",
+    to: "detail",
     number: "4",
     nameUser: "niya",
     EmailUser: "niya@gmail.com",
@@ -40,18 +40,26 @@ const ComponentTableUser = () => {
       <table className="min-w-full  text-right text-sm text-gray-700">
         <thead className="bg-gray-100 text-gray-700 ">
           <tr>
-            <th  className="w-12 px-4 py-3 font-medium hidden sm:table-cell">ردیف</th>
-            <th  className="w-12 px-4 py-3 font-medium hidden  sm:table-cell">کاربر</th>
+            <th className="w-12 px-4 py-3 font-medium hidden sm:table-cell">
+              ردیف
+            </th>
+            <th className="w-12 px-4 py-3 font-medium hidden  sm:table-cell">
+              کاربر
+            </th>
             <th className="w-12 px-4 py-3 font-medium  sm:table-cell">ایمیل</th>
-            <th  className="w-12 px-4 py-3 font-medium  sm:table-cell">نقش</th>
-            <th  className="w-12 px-4 py-3 font-medium  sm:table-cell">جزعیات</th>
+            <th className="w-12 px-4 py-3 font-medium  sm:table-cell">نقش</th>
+            <th className="w-12 px-4 py-3 font-medium  sm:table-cell">
+              جزعیات
+            </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-100">
           {coulmn.map((item) => (
             <tr className="hover:bg-gray-300 transition-colors  active:bg-gray-400">
               <td className="px-4 py-3 hidden sm:table-cell">{item.number}</td>
-              <td className="px-4 py-3 hidden sm:table-cell">{item.nameUser}</td>
+              <td className="px-4 py-3 hidden sm:table-cell">
+                {item.nameUser}
+              </td>
               <td className="px-4 py-3 ">{item.EmailUser}</td>
               <td className="px-4 py-3 text-green-600 font-medium">
                 {item.role}
