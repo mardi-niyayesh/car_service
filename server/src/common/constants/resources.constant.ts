7200be4bc11085f74e6df09cc0e40941f2dae016
homow <homowsh897@gmail.com>
2026-03-24 04:14:39 +0330

    feat(type) add a new for find delegate data in permission.guard

--- server/src/common/constants/resources.constant.ts
+++ server/src/common/constants/resources.constant.ts
@@ -1,3 +1,13 @@
 import {Prisma} from "@/modules/prisma/generated/client";
 
 export type Resource = Uncapitalize<Prisma.ModelName>;
+
+export type PrismaModels = Prisma.TypeMap["meta"]['modelProps'];
+
+export type FindDynamicDelegate = {
+  creator: string;
+};
+
+export type DynamicDelegate = {
+  findUnique: (args: { where: { id: string | undefined } }) => Promise<undefined | FindDynamicDelegate>;
+};
