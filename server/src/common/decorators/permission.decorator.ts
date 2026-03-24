ed3cb550a050c2be4c08db50f51623fc0970aafd
homow <homowsh897@gmail.com>
2026-03-24 03:32:16 +0330

    refactor(decorator) add resource and owner for ownership permission guard

--- server/src/common/decorators/permission.decorator.ts
+++ server/src/common/decorators/permission.decorator.ts
@@ -1,13 +1,15 @@
-import {PermissionsType} from "@/common";
 import {SetMetadata} from "@nestjs/common";
+import {PermissionsType, Resource} from "@/common";
 
 export const PERMISSION_METADATA = "PERMISSIONS";
 
 export interface PermissionDecoratorParams {
   requiredAll?: boolean;
   permissions: PermissionsType[];
+  owner?: boolean;
+  resource?: Resource;
 }
 
-export function Permission({requiredAll = false, permissions}: PermissionDecoratorParams) {
-  return SetMetadata(PERMISSION_METADATA, {requiredAll, permissions});
+export function Permission({requiredAll, permissions, owner, resource}: PermissionDecoratorParams) {
+  return SetMetadata(PERMISSION_METADATA, {requiredAll, permissions, owner, resource});
 }
