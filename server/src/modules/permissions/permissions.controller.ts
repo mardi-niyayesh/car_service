099a207729cb9b61bc3b63c2814e8d4b45c5c900
homow <homowsh897@gmail.com>
2026-03-26 06:14:44 +0330

    feat(module) add Permissions.Service to permissions.module

--- server/src/modules/permissions/permissions.controller.ts
+++ server/src/modules/permissions/permissions.controller.ts
@@ -1,10 +1,14 @@
 import {Controller, Get} from "@nestjs/common";
+import {Permission, PERMISSIONS} from "@/common";
 import {PermissionsService} from "./permissions.service";
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
+  @Permission({
+    permissions: [PERMISSIONS.PERMISSION_VIEW]
+  })
   @Get()
   test(): string {
     return this.permissionsService.findAll();
