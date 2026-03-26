f7d7a151b351d8d5c1a58d191d7d184217d5d996
homow <homowsh897@gmail.com>
2026-03-26 20:22:38 +0330

    feat(Model) add new model for category table

---
+++ server/prisma/migrations/20260326165139_add_category_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "slug" VARCHAR(150) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_slug_key" ON "categories"("slug");
