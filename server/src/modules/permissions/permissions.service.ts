a7bea841893d2a3fee2566c8657e536255f55ccf
homow <homowsh897@gmail.com>
2026-03-26 06:39:25 +0330

    feat(service) completed findAll permissions with pagination

--- server/src/modules/permissions/permissions.service.ts
+++ server/src/modules/permissions/permissions.service.ts
@@ -1,8 +1,34 @@
 import {Injectable} from "@nestjs/common";
+import {ApiResponse} from "@/types";
+import {PrismaService} from "@/modules/prisma/prisma.service";
+import {Permission, Prisma} from "@/modules/prisma/generated/client";
+import {getSafeSqlPaginate, PaginationValidatorType} from "@/common";
 
 @Injectable()
 export class PermissionsService {
-  findAll(): string {
-    return 'test all permissions';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(pagination: PaginationValidatorType): Promise<ApiResponse<{ permissions: Permission[] }>> {
+    const {orderBy, offset, limit} = getSafeSqlPaginate(pagination);
+
+    const result = await this.prisma.$queryRaw<Permission[]>(
+      Prisma.sql`
+        SELECT p.id,
+               p.name,
+               p.description,
+               p.created_at,
+               p.updated_at
+        FROM permissions p
+        GROUP BY p.id
+        ORDER BY p.created_at ${Prisma.sql([orderBy])}
+        LIMIT ${limit} OFFSET ${offset}`
+    );
+
+    return {
+      message: 'permissions successfully found',
+      data: {
+        permissions: result
+      }
+    };
   }
 }
