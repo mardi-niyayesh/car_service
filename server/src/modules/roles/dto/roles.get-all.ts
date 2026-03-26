182384f49de904126788e317bc5b92435dee9074
homow <homowsh897@gmail.com>
2026-03-27 00:35:51 +0330

    refactor(swagger) response example

--- server/src/modules/roles/dto/roles.get-all.ts
+++ server/src/modules/roles/dto/roles.get-all.ts
@@ -1,14 +1,15 @@
-import {RoleResponse} from "@/types";
 import {findOneRoleResponse} from "./roles.get";
 import {getBaseOkResponseSchema} from "@/common";
+import {FindAllRolesRes} from "@/modules/roles/roles.service";
 
 /** ok example for find all roles */
-export class FindAllRolesOkRes extends getBaseOkResponseSchema<{ roles: RoleResponse[] }>({
+export class FindAllRolesOkRes extends getBaseOkResponseSchema<FindAllRolesRes>({
   path: "roles?orderBy=desc&limit=5&page=1",
   response: {
     message: "roles successfully found.",
     data: {
-      roles: Array.from({length: 5}, () => findOneRoleResponse.role)
+      roles: Array.from({length: 5}, () => findOneRoleResponse.role),
+      rolesCount: 20
     }
   }
 }) {}
