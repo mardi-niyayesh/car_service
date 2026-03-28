f8b591fbc75188b6fb733af2448e3d83c395b29b
homow <homowsh897@gmail.com>
2026-03-28 06:07:08 +0330

    feat(controller) add permission and api swagger barrer

--- server/src/modules/category/category.controller.ts
+++ server/src/modules/category/category.controller.ts
@@ -1,10 +1,16 @@
+import {ApiBearerAuth} from "@nestjs/swagger";
 import {Controller, Get} from "@nestjs/common";
+import {Permission, PERMISSIONS} from "@/common";
 import {CategoryService} from "./category.service";
 
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @Permission({
+    permissions: [PERMISSIONS.CATEGORY_CREATE]
+  })
+  @ApiBearerAuth("accessToken")
   @Get()
   create() {
     return this.categoryService.create();
