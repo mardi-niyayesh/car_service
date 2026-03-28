45048328ed056626d84a968ede10d58a861f5613
homow <homowsh897@gmail.com>
2026-03-28 09:40:51 +0330

    refactor(swagger) add description

--- server/src/modules/category/category.controller.ts
+++ server/src/modules/category/category.controller.ts
@@ -1,11 +1,12 @@
 import * as CategoryDto from "./dto";
 import {CategoryService} from "./category.service";
-import {ApiBearerAuth, ApiBody} from "@nestjs/swagger";
 import {Body, Controller, Get, Post} from "@nestjs/common";
 import {Permission, PERMISSIONS, Public, ZodPipe} from "@/common";
+import {ApiBearerAuth, ApiBody, ApiOperation, ApiTags} from "@nestjs/swagger";
 
 @Public()
 @Controller('categories')
+@ApiTags('Categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
@@ -14,11 +15,20 @@ export class CategoryController {
     return 'find all categories';
   }
 
+  /** create a new category
+   * - only roles with permission (owner.all or category.create) can accessibility to this route
+   */
   @Permission({
     permissions: [PERMISSIONS.CATEGORY_CREATE]
   })
   @ApiBearerAuth("accessToken")
   @Post()
+  @ApiOperation({
+    summary: "create a new category",
+    description: `Create a category
+    only roles with permission (owner.all or category.create) can accessibility to this route`,
+    tags: ['Categories']
+  })
   @ApiBody({type: CategoryDto.CreateCategoryDto})
   create(
     @Body(new ZodPipe(CategoryDto.CreateCategoryValidator)) body: CategoryDto.CreateCategoryType,
