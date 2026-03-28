cb242aa8b9354f8c01a2c3b25525330f413cb088
homow <homowsh897@gmail.com>
2026-03-28 06:17:08 +0330

    feat(controller) add a endpoint for get all categories

--- server/src/modules/category/category.controller.ts
+++ server/src/modules/category/category.controller.ts
@@ -1,17 +1,23 @@
 import {ApiBearerAuth} from "@nestjs/swagger";
-import {Controller, Get} from "@nestjs/common";
-import {Permission, PERMISSIONS} from "@/common";
 import {CategoryService} from "./category.service";
+import {Controller, Get, Post} from "@nestjs/common";
+import {Permission, PERMISSIONS, Public} from "@/common";
 
+// @Public()
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAll() {
+    return 'find all categories';
+  }
 
   @Permission({
     permissions: [PERMISSIONS.CATEGORY_CREATE]
   })
   @ApiBearerAuth("accessToken")
-  @Get()
+  @Post()
   create() {
     return this.categoryService.create();
   }
