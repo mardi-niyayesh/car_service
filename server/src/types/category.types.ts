26492d779b8d03cd16fa53c1c1063b80e37c6a64
homow <homowsh897@gmail.com>
2026-03-30 15:21:17 +0330

    feat(type) add a new type for categories response

--- server/src/types/category.types.ts
+++ server/src/types/category.types.ts
@@ -1,6 +1,15 @@
 import {Category} from "@/modules/prisma/generated/client";
+import {ListWithCount} from "@/types/response.types";
 
 /** one category response */
 export interface CategoryResponse {
   category: Category;
 }
+
+/** type of list category response */
+export interface CategoriesResponse {
+  categories: Category[];
+}
+
+/** type of list category response with count */
+export type CategoriesResponseWithCount = ListWithCount<CategoriesResponse>;
