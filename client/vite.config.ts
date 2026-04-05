2d56f34425f9b6df4245ac4f605bd1928cd4d154
niyayesh <niyayeshammardii@gmail.com>
2026-04-05 13:15:32 -0700

    add rewrite and refactor

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
+          rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
 
         configure: (proxy, _options) => {
           proxy.on("proxyReq", (proxyReq, req, _res) => {
