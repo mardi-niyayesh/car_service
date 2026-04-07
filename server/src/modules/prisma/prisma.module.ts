39e806f2e90dd7abf43a240f860a959450ab60f5
homow <homowsh897@gmail.com>
2026-04-07 17:41:05 +0330

    refactor(prisma) add ConfigModule to Prisma

--- server/src/modules/prisma/prisma.module.ts
+++ server/src/modules/prisma/prisma.module.ts
@@ -1,8 +1,10 @@
+import {ConfigModule} from "@nestjs/config";
 import {Global, Module} from '@nestjs/common';
 import {PrismaService} from './prisma.service';
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
