e8fcf75a7f9a067a5a1dbffe61ac8694e2869d23
homow <homowsh897@gmail.com>
2026-04-11 18:59:17 +0330

    refactor(structure) move role types to role file

--- server/src/types/role.types.ts
+++ server/src/types/role.types.ts
@@ -1,6 +1,6 @@
 import {UserAccess} from "@/types/request.types";
 import {ListWithCount} from "@/types/response.types";
-import type {Role} from "@/modules/prisma/generated/client";
+import {Prisma, Role} from "@/modules/prisma/generated/client";
 
 /** role with permission[] type */
 export type RoleResponse = Role & {
@@ -32,3 +32,15 @@ export interface RolePermissionPolicyParams {
   permissions: string[];
   actionPermissions: UserAccess['permissions'];
 }
+
+/** role include payload in prisma */
+export type RoleIncludePayload = {
+  include: {
+    rolePermissions: {
+      include: { permission: true }
+    }
+  }
+}
+
+/** role full include type in prisma */
+export type RoleIncludeType = Prisma.RoleGetPayload<RoleIncludePayload>;
--- server/src/types/user.types.ts
+++ server/src/types/user.types.ts
@@ -1,5 +1,5 @@
 import {Prisma, User} from "@/modules/prisma/generated/client";
-import {UserAccess, RolesAndPermissions, ListWithCount} from "@/types";
+import {UserAccess, RolesAndPermissions, ListWithCount, RoleIncludePayload} from "@/types";
 
 /** User type without password */
 export type SafeUser = Omit<User, "password">;
@@ -19,18 +19,6 @@ export interface LoginResponse extends UserResponse {
 
 /** login user type */
 export type LoginUserSchemaType = { user: SafeUser & RolesAndPermissions; accessToken: string; };
-
-/** role include payload in prisma */
-type RoleIncludePayload = {
-  include: {
-    rolePermissions: {
-      include: { permission: true }
-    }
-  }
-}
-
-/** role full include type in prisma */
-export type RoleIncludeType = Prisma.RoleGetPayload<RoleIncludePayload>;
 
 /** user full role type in prisma */
 export type UserRoleIncludeType = Prisma.UserGetPayload<{
