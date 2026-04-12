2611998cdb64291f424597939dcf618b7aa59ed8
homow <homowsh897@gmail.com>
2026-04-13 03:19:25 +0330

    refactor(decorator) add validator type for uuid param

--- server/src/common/decorators/permission.decorator.ts
+++ server/src/common/decorators/permission.decorator.ts
@@ -1,16 +1,27 @@
+import z from "zod";
 import {SetMetadata} from "@nestjs/common";
 import {PermissionsType, PrismaModels} from "@/common";
 
 export const PERMISSION_METADATA = "PERMISSIONS";
 
-export interface PermissionDecoratorParams<T extends Record<string, unknown> = never> {
+export interface PermissionDecoratorParams<T extends Record<string, unknown> = never, U extends z.ZodTypeAny = never> {
   owner?: boolean;
   requiredAll?: boolean;
   resource?: PrismaModels;
   permissions: PermissionsType[];
   include?: T extends never ? never : T;
+  validatorParam?: U;
 }
 
-export function Permission<T extends Record<string, unknown> = never>({requiredAll, permissions, owner, resource, include}: PermissionDecoratorParams<T>) {
-  return SetMetadata(PERMISSION_METADATA, {requiredAll, permissions, owner, resource, include});
+export function Permission<T extends Record<string, unknown> = never, U extends z.ZodTypeAny = never>(
+  {
+    requiredAll,
+    permissions,
+    owner,
+    resource,
+    include,
+    validatorParam,
+  }: PermissionDecoratorParams<T, U>
+) {
+  return SetMetadata(PERMISSION_METADATA, {requiredAll, permissions, owner, resource, include, validatorParam});
 }
