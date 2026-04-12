02816eb0114241abc6fda9f31e3541ea381182da
homow <homowsh897@gmail.com>
2026-04-12 23:49:09 +0330

    feat(util) add a new for get path from node runtime

--- server/src/lib/config/common.ts
+++ server/src/lib/config/common.ts
@@ -1,1 +1,11 @@
+import {join} from "path";
 export const isProduction: boolean = process.env.NODE_ENV === "production";
+
+/** get path from node runtime root(process)
+ * @param path
+ * @returns string
+ * @return **join(process.cwd(), ...path);**
+ * */
+export function getPath(...path: string[]): string {
+  return join(process.cwd(), ...path);
+}
