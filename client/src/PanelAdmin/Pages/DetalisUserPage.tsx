f310b505a176a11c703756ab8cf79a60854f94d7
niyayesh <niyayeshammardii@gmail.com>
2026-04-20 06:09:06 -0700

    refactor ui

--- client/src/PanelAdmin/Pages/DetalisUserPage.tsx
+++ client/src/PanelAdmin/Pages/DetalisUserPage.tsx
@@ -1,7 +1,5 @@
 //components
 import ComponentDatailUser from "../Components/ComponentDatailUser";
-//icon or logo
-import ArrowLeft from "../../../assets/Arrowleft.png";
 //
 import { Link } from "react-router-dom";
 
@@ -9,18 +7,16 @@ const DetalisUserPage = () => {
   return (
     <div className="flex flex-col md:flex-row  md:gap-4 p-3 md:p-4 min-h-screen">
       <div className="flex-1">
-        {/* header User*/}
-        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between  p-3 sm:p-4">
-          <p className="text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
-            جزعیات کاربر
-          </p>
+        <div className="mb-4">
           <Link to="/panel/users">
-            <img
-              src={ArrowLeft}
-              alt="ArrowLeft"
-              className="w-10 h-10  rounded-4xl p-2  border border-gray-600"
-            />
+            <span className="hover:text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
+              کاربران/
+            </span>
           </Link>
+
+          <span className="hover:text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
+            جزعیات کابران
+          </span>
         </div>
         {/*Components CategoryDatailUser */}
         <div className="flex flex-col gap-4 ">
