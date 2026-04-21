8f1b41d80a35d5d6d7918a5c0ba51c95050fb2eb
niyayesh <niyayeshammardii@gmail.com>
2026-04-21 08:01:16 -0700

    update component

--- client/src/PanelAdmin/Pages/UsersPage.tsx
+++ client/src/PanelAdmin/Pages/UsersPage.tsx
@@ -12,21 +12,6 @@ const UsersPage = () => {
           </p>
         </div>
 
-        {/* category Users*/}
-        <div className="flex-1 ">
-          <div className="flex flex-wrap gap-3 sm:gap-4 mb-8">
-            <button className="bg-white text-[#9A9A9A] text-sm sm:text-base font-medium px-4 sm:px-6 py-2.5 sm:py-3 border-2 border-[#C2C2C2] rounded-xl hover:border-green-500 hover:text-green-600 transition-all duration-300 shadow-sm hover:shadow-md flex items-center gap-2">
-              <span> کاربران مدیر</span>
-            </button>
-
-            <button className="bg-white text-[#9A9A9A] text-sm sm:text-base font-medium px-4 sm:px-6 py-2.5 sm:py-3 border-2 border-[#C2C2C2] rounded-xl hover:border-yellow-500 hover:text-yellow-600 transition-all duration-300 shadow-sm hover:shadow-md flex items-center gap-2">
-              <span> کاربران مسدود</span>
-            </button>
-            <button className="bg-white text-[#9A9A9A] text-sm sm:text-base font-medium px-4 sm:px-6 py-2.5 sm:py-3 border-2 border-[#C2C2C2] rounded-xl hover:border-[#4A90E2] hover:text-[#4A90E2]  transition-all duration-300 shadow-sm hover:shadow-md flex items-center gap-2">
-              <span> همه کاربران </span>
-            </button>
-          </div>
-        </div>
         {/*Components TableUser */}
         <div dir="rtl" className="p-4">
           <ComponentTableUser />
