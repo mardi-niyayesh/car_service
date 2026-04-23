0fd8dc988ff9888b806744cff85d60907212fc4f
niyayesh <niyayeshammardii@gmail.com>
2026-04-23 09:52:07 -0700

    creat table to All roles

--- client/src/PanelAdmin/Pages/RolesPage.tsx
+++ client/src/PanelAdmin/Pages/RolesPage.tsx
@@ -1,8 +1,14 @@
 import axiosClient from "../../services/axiosClient";
 //hooks
 import { useState, useEffect } from "react";
+type RoleType = {
+  id: string;
+  name: string;
+  descriptions: string;
+  permissions: string[] | null;
+};
 const RolesPage = () => {
-  const [Roles, setRoles] = useState();
+  const [Roles, setRoles] = useState<RoleType[]>([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(10);
@@ -12,12 +18,13 @@ const RolesPage = () => {
       const response = await axiosClient.get(
         `/roles?order=desc&limit=10&page=${page}`,
       );
-      console.log("response to Roles :", response);
+      console.log("response to request :", response);
       const getAllRoles = response.data.response.data.roles;
-      const getCount = response.data.response.data.count;
-      const tota = Math.ceil(getCount / 10);
       console.log("response get alll roles :", getAllRoles);
+      const getCount = response.data.response.data.count;
       console.log("get count all roles :", getCount);
+      const tota = Math.ceil(getCount / 10);
+
       setTotalPage(tota);
       setRoles(getAllRoles);
     } catch (err) {
@@ -28,19 +35,64 @@ const RolesPage = () => {
   };
   useEffect(() => {
     fetchGetRoles();
-  }, []);
+  }, [page]);
   return (
-    <>
+    <div>
       {loading ? (
-        <div> در حال دریافت همه رول ها ...</div>
+        <div> در حال گرفتن همه ی نقش ها ...</div>
       ) : (
-        
-          {Roles.map((rol) => {
-            <div key={rol.id}>{rol.name }</div>;
-          })}
-       
+        <>
+          <div className="rounded-lg shadow-sm border border-gray-200 bg-white">
+            <table className="min-w-full text-right text-sm text-gray-700">
+              <thead className="bg-gray-100 text-gray-700">
+                <tr>
+                  <th className="px-4 py-3 font-medium hidden sm:table-cell">
+                    ردیف
+                  </th>
+                  <th className="px-4 py-3 font-medium sm:table-cell">
+                    اسم نقش
+                  </th>
+                  <th className="px-4 py-3 font-medium sm:table-cell">
+                    توضیحات
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {Roles.length === 0 ? (
+                  <tr>
+                    <td colSpan={3} className="px-4 py-3 text-center">
+                      هیچ نقشی یافت نشد.
+                    </td>
+                  </tr>
+                ) : (
+                  Roles.map((rol, index) => {
+                    return (
+                      <tr
+                        key={rol.id}
+                        className="hover:bg-gray-50 transition-colors border-b border-gray-100"
+                      >
+                        <td className="px-4 py-3 hidden sm:table-cell">
+                          {(page - 1) * 10 + index + 1}
+                        </td>
+                        <td className="px-4 py-3 text-green-600 sm:table-cell">
+                          {rol.name}
+                        </td>
+                        <td className="px-4 py-3 text-blue-400">
+                          {Array.isArray(rol.permissions)
+                            ? rol.permissions.join("  , ")
+                            : rol.permissions}
+                        </td>
+                      </tr>
+                    );
+                  })
+                )}
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
