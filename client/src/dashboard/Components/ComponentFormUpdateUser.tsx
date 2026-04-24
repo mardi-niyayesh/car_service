1daa702434439e2cc5a279d3b1c462e1d7b5515b
niyayesh <niyayeshammardii@gmail.com>
2026-04-24 08:47:38 -0700

    add success Modal and warning Modal to response

--- client/src/dashboard/Components/ComponentFormUpdateUser.tsx
+++ client/src/dashboard/Components/ComponentFormUpdateUser.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useUpdateUser } from "../Api/ApiUpdatUser";
 //Modal
 import SuccessModal from "../../components/common/SuccessModal";
-import ErrorModal from "../../components/common/ErrorModal";
+import WarningModal from "../../components/common/WarningModal ";
 import { useState } from "react";
 
 type FormData = {
@@ -13,6 +13,12 @@ type FormData = {
 
 const ComponentFormUpdateUser = () => {
   const FetchUpdateUser = useUpdateUser();
+  //success Modal
+  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  //Warning Modal
+  const [isWarningOpen, setIsWarningOpen] = useState(false);
+  const [WarningMessage, setWarningMessage] = useState("");
 
   const {
     register,
@@ -28,7 +34,13 @@ const ComponentFormUpdateUser = () => {
         age: Number(data.age),
       });
       if (result.ok) {
-        console.log("success:", result);
+        console.log("success in updat information user:", result);
+        setSuccessMessage(result.message);
+        setIsSuccessOpen(true);
+        reset();
+      } else {
+        setWarningMessage(result.message);
+        setIsWarningOpen(true);
       }
 
       reset();
@@ -38,88 +50,103 @@ const ComponentFormUpdateUser = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="border border-[#EDEDED] rounded-xl bg-white shadow-sm"
-    >
-      <div className="p-6 bg-white">
-        <div className="mb-4">
-          <Link to="/dashboard/Profile">
-            <span className="text-gray-800 text-[20px] hover:text-blue-600 font-medium cursor-pointer  mb-4">
-              پروفایل/
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="border border-[#EDEDED] rounded-xl bg-white shadow-sm"
+      >
+        <div className="p-6 bg-white">
+          <div className="mb-4">
+            <Link to="/dashboard/Profile">
+              <span className="text-gray-800 text-[20px] hover:text-blue-600 font-medium cursor-pointer  mb-4">
+                پروفایل/
+              </span>
+            </Link>
+            <span className="text-gray-800 text-[20px] font-medium mb-4">
+              فرم ویرایش کاربر
             </span>
-          </Link>
-          <span className="text-gray-800 text-[20px] font-medium mb-4">
-            فرم ویرایش کاربر
-          </span>
-        </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4 mb-8">
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              نام کامل
-            </label>
-            <input
-              type="text"
-              {...register("display_name", {
-                required: "نام الزامی است",
-                minLength: {
-                  value: 3,
-                  message: "نام باید حداقل ۳ حرف داشته باشد",
-                },
-                pattern: {
-                  value: /^[\u0600-\u06FFa-zA-Z\s]+$/,
-                  message: "نام باید فقط شامل حروف باشد",
-                },
-              })}
-              placeholder="کاربر جدید"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-            />
-            {errors.display_name && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.display_name.message}
-              </p>
-            )}
           </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              سن کاربر
-            </label>
-            <input
-              type="tel"
-              {...register("age", {
-                required: "سن الزامی است",
-                min: {
-                  value: 0,
-                  message: "سن نباید منفی باشد",
-                },
-                max: {
-                  value: 120,
-                  message: "سن نباید بیشتر از 120 باشد",
-                },
-                pattern: {
-                  value: /^[0-9]+$/,
-                  message: "سن باید عدد باشد",
-                },
-              })}
-              placeholder="سن کاربر"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-            />
-            {errors.age && (
-              <p className="text-red-500 text-sm mt-1">{errors.age.message}</p>
-            )}
-          </div>
-        </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-4 mb-8">
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                نام کامل
+              </label>
+              <input
+                type="text"
+                {...register("display_name", {
+                  required: "نام الزامی است",
+                  minLength: {
+                    value: 3,
+                    message: "نام باید حداقل ۳ حرف داشته باشد",
+                  },
+                  pattern: {
+                    value: /^[\u0600-\u06FFa-zA-Z\s]+$/,
+                    message: "نام باید فقط شامل حروف باشد",
+                  },
+                })}
+                placeholder="کاربر جدید"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
+              />
+              {errors.display_name && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.display_name.message}
+                </p>
+              )}
+            </div>
 
-        <button
-          type="submit"
-          className="py-3 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300 w-full md:w-auto"
-        >
-          ثبت اطلاعات
-        </button>
-      </div>
-    </form>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                سن کاربر
+              </label>
+              <input
+                type="tel"
+                {...register("age", {
+                  required: "سن الزامی است",
+                  min: {
+                    value: 0,
+                    message: "سن نباید منفی باشد",
+                  },
+                  max: {
+                    value: 120,
+                    message: "سن نباید بیشتر از 120 باشد",
+                  },
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "سن باید عدد باشد",
+                  },
+                })}
+                placeholder="سن کاربر"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
+              />
+              {errors.age && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.age.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="py-3 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300 w-full md:w-auto"
+          >
+            ثبت اطلاعات
+          </button>
+        </div>
+      </form>
+      <SuccessModal
+        isOpen={isSuccessOpen}
+        onClose={() => setIsSuccessOpen(false)}
+        message={successMessage}
+      />
+
+      <WarningModal
+        isOpen={isWarningOpen}
+        onClose={() => setIsWarningOpen(false)}
+        message={WarningMessage}
+      />
+    </>
   );
 };
 
