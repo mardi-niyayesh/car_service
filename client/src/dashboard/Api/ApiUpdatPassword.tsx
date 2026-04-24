ce096be9ad012ced947a5e6630bb9460bfa8fa23
niyayesh <niyayeshammardii@gmail.com>
2026-04-24 09:20:32 -0700

    refactor api update password

--- client/src/dashboard/Api/ApiUpdatPassword.tsx
+++ client/src/dashboard/Api/ApiUpdatPassword.tsx
@@ -16,18 +16,27 @@ export const useUpdatePassword = () => {
       if (response.status === 200) {
         console.log("Update successful, status:", response.data);
 
-        alert("رمزعبور کاربر با موفقیت به‌روزرسانی شد.");
-        console.log("Update successful, status:", response.data);
-        return response.data;
+        return {
+          ok: true,
+          message: " رمز عبور شما با موفقیت به روز رسانی شد",
+        };
       }
+      return {
+        ok: false,
+        message: "خطایی در به روز رسانی رمز عبور رخ داد ",
+      };
     } catch (err) {
+      console.error("Error updatePassword :", err);
       const status = err.response?.status;
 
       if (status === 401) {
-        alert("رمز عبور فعلی وارد شده صحیح نیست");
+        return {
+          ok: false,
+          message: " رمز عبور فعلی وارد شده صحیح نیست !  ",
+        };
+      } else {
+        return { ok: false, message: "خطایی در سرور رخ داده است." };
       }
-      console.error("Error updatePassword :", err);
-      throw err;
     }
   };
 
