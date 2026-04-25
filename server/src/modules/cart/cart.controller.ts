d58a7b702e89d1399e4558696b2f23b571c346d5
homow <homowsh897@gmail.com>
2026-04-25 09:44:43 +0330

    feat(endpoint) add a new for get self cart

--- server/src/modules/cart/cart.controller.ts
+++ server/src/modules/cart/cart.controller.ts
@@ -1,4 +1,22 @@
-import {Controller} from '@nestjs/common';
+import {CartService} from "./cart.service";
+import type {AccessRequest} from "@/types";
+import {ApiBearerAuth} from "@nestjs/swagger";
+import {Permission, PERMISSIONS} from "@/common";
+import {Controller, Get, Req} from '@nestjs/common';
 
 @Controller('carts')
-export class CartController {}
+@ApiBearerAuth("accessToken")
+@Permission({
+  permissions: [PERMISSIONS.USER_SELF]
+})
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @Get()
+  getCart(
+    @Req() req: AccessRequest
+  ) {
+    console.log(req.user);
+    return 'cart successfully find.';
+  }
+}
