7bb7320a4e3dc7035a3ac24a8a70b07236d7e2e8
homow <homowsh897@gmail.com>
2026-04-25 11:31:17 +0330

    feat(endpoint) add a new for add car rent to cart

--- server/src/modules/cart/cart.controller.ts
+++ server/src/modules/cart/cart.controller.ts
@@ -1,8 +1,9 @@
 import {CartService} from "./cart.service";
 import {ApiBearerAuth} from "@nestjs/swagger";
 import * as CartDecorator from "./decorators";
-import {Permission, PERMISSIONS} from "@/common";
-import {Controller, Get, Req} from '@nestjs/common';
+import {CarSlugValidator} from "@/modules/car/dto";
+import {Permission, PERMISSIONS, ZodPipe} from "@/common";
+import {Controller, Get, Param, Post, Req} from '@nestjs/common';
 import type {AccessRequest, ApiResponse, CartResponse} from "@/types";
 
 @Controller('carts')
@@ -22,5 +23,12 @@ export class CartController {
     @Req() req: AccessRequest
   ): Promise<ApiResponse<CartResponse>> {
     return this.cartService.getCart(req.user.userId, req.user);
+  }
+
+  @Post(":slug")
+  addToCart(
+    @Param('slug', new ZodPipe(CarSlugValidator)) slug: string,
+  ) {
+    return slug;
   }
 }
