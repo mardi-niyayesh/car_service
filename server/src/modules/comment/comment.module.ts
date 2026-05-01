29131a0c6e2304fcd241e85f1a91fdd096edefbd
homow <homowsh897@gmail.com>
2026-05-01 20:27:10 +0330

    feat(service) add a new comments

--- server/src/modules/comment/comment.module.ts
+++ server/src/modules/comment/comment.module.ts
@@ -1,9 +1,10 @@
 import {Module} from "@nestjs/common";
+import {CommentService} from "./comment.service";
 import {CommentController} from "./comment.controller";
 
 @Module({
-  exports: [],
-  providers: [],
+  exports: [CommentService],
+  providers: [CommentService],
   controllers: [CommentController],
 })
 export class CommentModule {}
