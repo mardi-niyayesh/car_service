fc0f0558f72baa26c3bf6e819b5d010c43cd84a6
niyayesh <niyayeshammardii@gmail.com>
2026-05-03 11:13:22 -0700

    deleat comments to components

--- client/src/PanelAdmin/Pages/CategoryPage.tsx
+++ client/src/PanelAdmin/Pages/CategoryPage.tsx
@@ -1,11 +1,9 @@
 import ComponentTableCategory from "../Components/ComponentTableCategory";
-//icone
 import locationadd from "../../../assets/location-add.png";
 const CategoryPage = () => {
   return (
     <div className="flex flex-col md:flex-row  md:gap-4 p-3 md:p-4 min-h-screen">
       <div className="flex-1">
-        {/* header category*/}
         <div className="flex flex-col sm:flex-row mb-10 items-start sm:items-center justify-between gap-3 sm:gap-0 p-3 sm:p-4 border border-[#EDEDED] rounded-xl bg-white shadow-sm">
           <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between  p-3 sm:p-4">
             <p className="text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
@@ -23,7 +21,6 @@ const CategoryPage = () => {
             </p>
           </div>
         </div>
-        {/*Components TableCategory */}
         <div className="flex flex-col gap-4 ">
           <ComponentTableCategory />
         </div>
