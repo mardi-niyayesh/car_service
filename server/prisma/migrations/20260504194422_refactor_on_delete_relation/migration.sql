0b3ecaed1f9cabd0265923cd295bb804d8d852a7
homow <homowsh897@gmail.com>
2026-05-04 23:14:44 +0330

    refactor(model) refactor on delete relation(comment model)

---
+++ server/prisma/migrations/20260504194422_refactor_on_delete_relation/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_car_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_parent_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "comments" DROP CONSTRAINT "comments_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "cars"("id") ON DELETE CASCADE ON UPDATE CASCADE;
