6154713a9610f7da33cc6f66e664a2ce7d14c9cd
niyayesh <niyayeshammardii@gmail.com>
2026-05-04 12:24:38 -0700

    handle logic create new category

--- client/src/PanelAdmin/Components/ComponentAddCategory.tsx
+++ client/src/PanelAdmin/Components/ComponentAddCategory.tsx
@@ -1,7 +1,51 @@
+import axiosClient from "../../services/axiosClient";
+import { useState } from "react";
+import SuccessModal from "../../components/common/SuccessModal";
+import WarningModal from "../../components/common/WarningModal ";
 const ComponentAddCategory = () => {
+  const [checkOwnership, setCheckOwnership] = useState(false);
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDescription, setCategoryDescription] = useState("");
+  const [categorySlug, setCategorySlug] = useState("");
+  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isWarningOpen, setIsWarningOpen] = useState(false);
+  const [WarningMessage, setWarningMessage] = useState("");
+
+  const CreateCategory = async () => {
+    try {
+      const response = await axiosClient.post(`/categories`, {
+        name: categoryName,
+        description: categoryDescription,
+        slug: categorySlug,
+        ownership: checkOwnership,
+      });
+      console.log("responst to creat category :", response.data);
+      setSuccessMessage("دسته بندی جدید با موفقیت ساخته شد");
+      setCategoryName("");
+      setCategoryDescription("");
+      setCategorySlug("");
+      setCheckOwnership(false);
+    } catch (err) {
+      console.log("Error in creat new category :", err);
+      setWarningMessage("لطفا مجدد تلاش کنید");
+    }
+  };
+  const handleOwnerShip = (e) => {
+    setCheckOwnership(e.target.checked);
+    console.log("ownership : ", e.target.checked);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSuccessMessage("");
+    setWarningMessage("");
+  };
   return (
     <>
-      <form className="border border-[#EDEDED] rounded-xl bg-white shadow-sm">
+      <form
+        className="border border-[#EDEDED] rounded-xl bg-white shadow-sm"
+        onSubmit={handleSubmit}
+      >
         <div className="p-6 bg-white">
           <p className="text-[#4b33b5] text-[20px] sm:text-[20px] md:text-[20px] font-bold mb-4">
             فرم اضافه کردن دسته بندی
@@ -9,9 +53,10 @@ const ComponentAddCategory = () => {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mb-8">
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
-                نام دسته بندی
+                نام دسته بندی <span className="text-red-500">*</span>
               </label>
               <input
+                onChange={(e) => setCategoryName(e.target.value)}
                 type="text"
                 placeholder=" نام..."
                 className="w-full px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
@@ -21,8 +66,10 @@ const ComponentAddCategory = () => {
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
                 توضیحات دسته بندی
+                <span className="text-gray-400 text-xs">(اختیاری)</span>
               </label>
               <input
+                onChange={(e) => setCategoryDescription(e.target.value)}
                 type="text"
                 placeholder="توضیحات... "
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
@@ -31,21 +78,24 @@ const ComponentAddCategory = () => {
 
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
-                لینک دسته بندی
+                لینک دسته بندی <span className="text-red-500">*</span>
               </label>
               <input
                 type="text"
                 placeholder="لینک..."
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
               />
+              <p className="text-xs text-gray-400 mt-1">
+                فقط حروف کوچک انگلیسی، اعداد و خط تیره
+              </p>
             </div>
 
             <div className="flex gap-2 items-center justify-start">
               <div className="flex items-center">
-                <input type="checkbox" />
+                <input type="checkbox" onClick={handleOwnerShip} />
               </div>
               <label className="block text-sm font-medium text-gray-700 mb-1">
-                مالکیت
+                مالکیت (Ownership)
               </label>
             </div>
           </div>
@@ -57,6 +107,18 @@ const ComponentAddCategory = () => {
           </button>
         </div>
       </form>
+
+      <SuccessModal
+        isOpen={isSuccessOpen}
+        onClose={() => setIsSuccessOpen(false)}
+        message={successMessage}
+      />
+
+      <WarningModal
+        isOpen={isWarningOpen}
+        onClose={() => setIsWarningOpen(false)}
+        message={WarningMessage}
+      />
     </>
   );
 };
