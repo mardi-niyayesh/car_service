8e6520d1ed46c8f30b2d01afe84ac05b5c4eb414
homow <homowsh897@gmail.com>
2026-05-05 00:37:10 +0330

    feat(service) add new for create comment

--- server/src/modules/comment/comment.service.ts
+++ server/src/modules/comment/comment.service.ts
@@ -1,4 +1,32 @@
+import {ApiResponse} from "@/types";
+import * as CommentDto from "./dto";
 import {Injectable} from "@nestjs/common";
+import {Comment} from "@/modules/prisma/generated/client";
+import {PrismaService} from "@/modules/prisma/prisma.service";
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(user_id: string, data: CommentDto.CreateCommentType): Promise<ApiResponse<{comment: Comment}>> {
+    try {
+      const comment = await this.prisma.comment.create({
+        data: {
+          ...data,
+          user_id
+        }
+      });
+
+      return {
+        message: 'comment created successfully.',
+        data: {
+          comment
+        }
+      };
+    } catch (e) {
+      console.log(e);
+
+      throw e;
+    }
+  }
+}
