a7d64fa7a69928fffdeb80aa5028c0eb28472326
niyayesh <niyayeshammardii@gmail.com>
2026-05-06 12:23:33 -0700

    fix Replace Tailwind CDN with npm installation for better performance . Closes#9

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,9 +1,12 @@
 import {defineConfig} from "vitest/config";
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+     tailwindcss(), 
   ],
   test: {
     mockReset: true,
