a1f1a616cf76ee78fe5a84a8e2803c2a55720969
niyayesh <niyayeshammardii@gmail.com>
2026-05-10 11:09:41 -0700

    refactor ui

--- client/src/dashboard/Components/ComponentFormUpdateUser.tsx
+++ client/src/dashboard/Components/ComponentFormUpdateUser.tsx
@@ -135,7 +135,7 @@ const ComponentFormUpdateUser = () => {
           </p>
           <button
             type="submit"
-            className="py-3 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300 w-full md:w-auto"
+            className="py-3 px-4 cursor-pointer bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300 w-full md:w-auto"
           >
             ثبت اطلاعات
           </button>
