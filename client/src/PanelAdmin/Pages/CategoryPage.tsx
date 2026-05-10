97264a75533abd37c49ed5b4e8386e279877486c
niyayesh <niyayeshammardii@gmail.com>
2026-05-10 11:08:36 -0700

    autorization to creat category

--- client/src/PanelAdmin/Pages/CategoryPage.tsx
+++ client/src/PanelAdmin/Pages/CategoryPage.tsx
@@ -1,6 +1,11 @@
 import ComponentTableCategory from "../Components/CategoryForm/ComponentTableCategory";
+import { useUser } from "../../hooks/useUser";
 import { Link } from "react-router-dom";
 const CategoryPage = () => {
+  const { hasPermission, hasRole } = useUser();
+  const hasCreatPermession =
+    hasPermission("category.creat") || hasRole("category_manager");
+
   return (
     <div className="flex flex-col md:flex-row  md:gap-4 p-3 md:p-4 min-h-screen">
       <div className="flex-1">
@@ -10,11 +15,13 @@ const CategoryPage = () => {
               دسته بندی
             </p>
           </div>
-          <Link to="/panel/category/CreatCategory">
-            <p className="text-[#194BF0] hover:bg-blue-200 bg-blue-100 p-2 rounded-xl text-[14px] md:text-[16px] font-medium whitespace-nowrap">
-             + ساختن دسته بندی جدید
-            </p>
-          </Link>
+          {hasCreatPermession && (
+            <Link to="/panel/category/CreatCategory">
+              <p className="text-[#194BF0] hover:bg-blue-200 bg-blue-100 p-2 rounded-xl text-[14px] md:text-[16px] font-medium whitespace-nowrap">
+                + ساختن دسته بندی جدید
+              </p>
+            </Link>
+          )}
         </div>
         <div className="flex flex-col gap-4 ">
           <ComponentTableCategory />
