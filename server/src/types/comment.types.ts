1c0a073e56b87b86fe69323cd5b766f7f3d327db
homow <homowsh897@gmail.com>
2026-05-11 23:48:24 +0330

    refactor(style) imports

--- server/src/types/comment.types.ts
+++ server/src/types/comment.types.ts
@@ -1,7 +1,7 @@
 import type {SafeCar} from "@/types/car.types";
 import type {SafeUser} from "@/types/user.types";
-import type {Comment} from "@/modules/prisma/generated/client";
 import {ListWithCount} from "@/types/response.types";
+import type {Comment} from "@/modules/prisma/generated/client";
 
 /** Comment with car and user response */
 export type CommentNUserNCar = Comment & {
