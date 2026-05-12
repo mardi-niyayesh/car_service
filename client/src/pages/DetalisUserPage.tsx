6e75c941dbc3d93ee212080cc1282774c444a75f
niyayesh <niyayeshammardii@gmail.com>
2026-05-12 13:40:09 -0700

    Changing a file's location to pages

---
+++ client/src/pages/DetalisUserPage.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import ComponentDatailUser from "../components/PanelAdmin/UserForm/ComponentDatailUser";
+const DetalisUserPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row  md:gap-4 p-3 md:p-4 min-h-screen">
+      <div className="flex-1">
+        <div className="mb-4">
+          <Link to="/panel/users">
+            <span className="hover:text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
+              کاربران/
+            </span>
+          </Link>
+
+          <span className="hover:text-[#4b33b5] text-[20px]  sm:text-[20px] md:text-[20px] font-bold">
+            جزعیات کابران
+          </span>
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <ComponentDatailUser />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetalisUserPage;
