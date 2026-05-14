e04ed5247bc68084d72006822e2359302a502380
niyayesh <niyayeshammardii@gmail.com>
2026-05-14 13:32:31 -0700

    refactor and change url import

---
+++ client/src/pages/AddressPages.tsx
@@ -0,0 +1,65 @@
+import locationadd from "../../../assets/location-add.png";
+
+const AddressPages = () => {
+  return (
+    <div className="w-full md:flex-1">
+      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-3 sm:gap-0 p-3 sm:p-4 border border-[#EDEDED] rounded-xl bg-white shadow-sm">
+        <p className="text-[#212121] text-[14px] sm:text-[16px] md:text-[18px] font-bold">
+          آدرس‌های من
+        </p>
+
+        <div className="flex items-center justify-center sm:justify-end gap-2 w-full sm:w-auto cursor-pointer bg-blue-50 sm:bg-transparent p-2 sm:p-0 rounded-lg">
+          <img
+            src={locationadd}
+            alt="locationadd"
+            className="w-4 h-4 md:w-5 md:h-5"
+          />
+          <p className="text-[#194BF0] text-[14px] md:text-[16px] font-medium whitespace-nowrap">
+            ثبت آدرس جدید
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-4 w-full">
+        {/* defult Address*/}
+        <div className="p-3 sm:p-4 border border-[#EDEDED] rounded-xl bg-white hover:shadow-md transition-all duration-200 w-full">
+          <div className="flex flex-col xs:flex-row justify-between items-start gap-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-[#212121] text-[14px] sm:text-[16px]">
+                آدرس
+              </p>
+              <span className="bg-blue-500 text-white text-[10px] px-2 py-0.5 rounded-full">
+                پیش‌فرض
+              </span>
+            </div>
+            <div className="flex items-center gap-3 w-full xs:w-auto justify-end">
+              <button className="text-blue-600 text-xs sm:text-sm cursor-pointer hover:font-bold transition-all duration-200">
+                ویرایش
+              </button>
+              <button className="text-red-500 text-xs sm:text-sm cursor-pointer hover:font-bold transition-all duration-200">
+                حذف
+              </button>
+            </div>
+          </div>
+
+          <p className="text-xs sm:text-sm text-gray-600 mt-2 leading-relaxed">
+            تهران، شهرک غرب، بلوار فرحزادی، خیابان سپیدار، پلاک ۴۵
+          </p>
+
+          <div className="flex flex-wrap items-center gap-3 mt-3 text-xs text-gray-400">
+            <span className="flex items-center gap-1">
+              <span className="font-medium text-gray-500">کد پستی:</span>
+              ۵۵۴۴۳۳۲۲۱۱
+            </span>
+            <span className="flex items-center gap-1">
+              <span className="font-medium text-gray-500"> ایمیل:</span>
+              test@gmail.com
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressPages;
---
+++ client/src/pages/LogoutPage.tsx
@@ -0,0 +1,148 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { LogoutUser } from "../dashboard/Api/ApiLogoutUser";
+import WarningModal from "../Modal/WarningModal ";
+import SuccessModal from "../Modal/SuccessModal";
+import ErrorModal from "../Modal/ErrorModal";
+import { useUser } from "../hooks/useUser";
+
+const LogoutPage = () => {
+  const navigate = useNavigate();
+  const { logout } = useUser();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+
+    setIsModalOpen(false);
+    setIsErrorModalOpen(false);
+
+    try {
+      const result = await LogoutUser();
+
+      if (!result) {
+        setErrorMessage("پاسخی از سرور دریافت نشد");
+        setIsErrorModalOpen(true);
+        setIsLoading(false);
+        return;
+      }
+
+      if (result.success === true) {
+        setModalMessage("شما با موفقیت از حساب کاربری خود خارج شدید");
+        logout();
+        setIsModalOpen(true);
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      } else if (result.statusCode === 401) {
+        setWarningMessage(
+          `نشست شما به پایان رسیده است . لطفا برای ادامه کار مجدد وارد حساب کاربری خود شوید`,
+        );
+        setIsWarningModalOpen(true);
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      } else if (result.statusCode === 403) {
+        setWarningMessage(
+          ` نشست شما در دستگاه دیگری پایان یافته است.
+        اگر خودتان وارد سیستم شده‌اید، لطفاً مجدداً وارد شوید.
+        اگر این کار را نکرده‌اید، برای امنیت حساب خود رمز عبور را تغییر دهید.`,
+        );
+        setIsWarningModalOpen(true);
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      } else if (result.statusCode === 500) {
+        setErrorMessage("خطای سرور. لطفاً بعداً دوباره تلاش کنید.");
+        setIsErrorModalOpen(true);
+      }
+    } catch (err) {
+      console.log("خطا :", err);
+      setErrorMessage("خطا در برقراری ارتباط با سرور. لطفا مجدد تلاش کنید");
+      setIsErrorModalOpen(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div
+      className="flex flex-col md:flex-row min-h-screen bg-gray-50"
+      dir="rtl"
+    >
+      <div className="flex-1 p-4 sm:p-6 md:p-8">
+        <div className="mb-6 md:mb-8">
+          <p className="text-[#212121] text-xl sm:text-2xl md:text-3xl font-bold pb-3 inline-block">
+            خروج
+          </p>
+        </div>
+
+        <div className="bg-white rounded-2xl shadow-lg p-6 sm:p-8 max-w-md mx-auto md:mx-0">
+          <div className="text-[#212121] sm:text-lg md:text-xl font-medium text-center mb-8">
+            برای خروج از حساب کاربری خود اطمینان دارید؟
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-4 justify-center">
+            <Link to="/dashboard">
+              <button className="text-[#DB1D27] border-2 border-[#DB1D27] px-6 sm:px-8 py-3 rounded-xl hover:text-white hover:bg-[#DB1D27] transition-all duration-300 font-medium text-sm sm:text-base w-full sm:w-auto">
+                انصراف
+              </button>
+            </Link>
+
+            <button
+              onClick={handleLogout}
+              disabled={isLoading}
+              className={`text-white bg-[#DB1D27] border-2 border-[#DB1D27] px-6 sm:px-8 py-3 rounded-xl hover:bg-white hover:text-[#DB1D27] transition-all duration-300 font-medium text-sm sm:text-base w-full sm:w-auto ${
+                isLoading ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+            >
+              {isLoading ? "در حال خروج..." : "خروج از حساب"}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isModalOpen && (
+        <SuccessModal
+          isOpen={isModalOpen}
+          message={modalMessage}
+          onClose={() => {
+            setIsModalOpen(false);
+            navigate("/login");
+          }}
+        />
+      )}
+
+      {isErrorModalOpen && (
+        <ErrorModal
+          isOpen={isErrorModalOpen}
+          message={errorMessage}
+          onClose={() => {
+            setIsErrorModalOpen(false);
+          }}
+        />
+      )}
+      <WarningModal
+        isOpen={isWarningModalOpen}
+        message={warningMessage}
+        onClose={() => {
+          setIsWarningModalOpen(false);
+        }}
+        title=" توجه"
+      />
+    </div>
+  );
+};
+
+export default LogoutPage;
