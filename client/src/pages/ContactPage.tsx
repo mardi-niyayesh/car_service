f66f03c5146b44330e4669e8334b5c81ff37d1b3
niyayesh <niyayeshammardii@gmail.com>
2026-05-23 12:17:22 -0700

    refactor

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -115,7 +115,7 @@ const ContactPage = () => {
                   <textarea
                     id="message"
                     rows="4"
-                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   ></textarea>
                 </div>
                 <div>
