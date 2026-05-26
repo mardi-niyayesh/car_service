a6440aa1667f9e6f9135d91c5787d69a17ee327a
niyayesh <niyayeshammardii@gmail.com>
2026-05-26 11:52:32 -0700

    add shart  to shoe description

--- client/src/ShopCart/Des3Car.tsx
+++ client/src/ShopCart/Des3Car.tsx
@@ -2,18 +2,22 @@ import { useProduct } from "../hooks/useProduct";
 import { useParams } from "react-router-dom";
 const Des3Car = () => {
   const { allProduct } = useProduct();
-  const { id } = useParams();
+  const { slug } = useParams();
 
-  const findProduct = allProduct.find((pro) => pro.id === id);
+  const findProduct = allProduct.find((pro) => pro.slug === slug);
   return (
-    <div className="bg-white rounded-2xl shadow-lg overflow-hidden border border-gray-100 p-5">
-      <h2 className="text-2xl font-bold text-blue-800 border-r-4 border-blue-600 pr-3 mb-4">
-        درباره ی ماشین
-      </h2>
-      <p className="text-gray-600 mb-6 text-sm leading-relaxed">
-        {findProduct?.description}
-      </p>
-    </div>
+    <>
+      {findProduct?.description && (
+        <div className="bg-white rounded-2xl shadow-lg overflow-hidden border border-gray-100 p-5">
+          <h2 className="text-2xl font-bold text-blue-800 border-r-4 border-blue-600 pr-3 mb-4">
+            درباره ی ماشین
+          </h2>
+          <p className="text-gray-600 mb-6 text-sm leading-relaxed">
+            {findProduct?.description}
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
