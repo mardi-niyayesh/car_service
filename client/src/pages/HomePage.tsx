87121e0958f28731d82fec1675d37931041828b0
niyayesh <niyayeshammardii@gmail.com>
2026-05-26 05:11:06 -0700

    delet component HeroBaner to HomePage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,10 @@
 import BanerHeader from "../components/Header/BanerHeader";
-import HeroBaner from "../components/Main/HeroBaner";
 import MainSite from "../components/Main/MainSite";
 
 const HomePage = () => {
   return (
     <>
       <BanerHeader />
-      <HeroBaner />
       <MainSite />
     </>
   );
