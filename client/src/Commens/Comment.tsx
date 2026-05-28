397a1cb14d8e0cedb3da6a22e0f84cb3da642c53
niyayesh <niyayeshammardii@gmail.com>
2026-05-28 13:53:29 -0700

    handle logic new comment and complet

--- client/src/Commens/Comment.tsx
+++ client/src/Commens/Comment.tsx
@@ -1,85 +1,155 @@
-const Comment = () => {
+import { useForm } from "react-hook-form";
+import axiosClient from "../services/axiosClient";
+import { useParams } from "react-router-dom";
+import { useProduct } from "../hooks/useProduct";
+import { useState } from "react";
+import SuccessModal from "../Modal/SuccessModal";
+import WarningModal from "../Modal/WarningModal ";
+type CommentForm = {
+  rate?: number;
+  content: string;
+  parent_id: null | string;
+};
+const Comment = (replyToId: null) => {
+  const { slug } = useParams();
+  console.log("slug :", slug);
+
+  const { allProduct } = useProduct();
+
+  const [loading, setLoading] = useState(false);
+  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [isWarningOpen, setIsWarningOpen] = useState(false);
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const {
+    register,
+  
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { content: "", rate: 5, parent_id: replyToId },
+  });
+
+  const product = allProduct.find((pro) => pro.slug === slug);
+  console.log("inform product :", product);
+ 
+  if (!product) {
+    return <div>محصول مورد نظر یافت نشد</div>;
+  }
+
+ const selectedProduct = product.id;
+
+  const onsubmit = async (data: CommentForm) => {
+    setLoading(true);
+    const payload: any = {
+      content: String(data.content).trim(),
+      rate: Number(data.rate ?? 5),
+    };
+
+    try {
+      const response = await axiosClient.post(
+        `comments/${selectedProduct}`,
+        payload,
+      );
+      console.log("response to creat comment :", response);
+      if (response.status === 201) {
+        setIsSuccessOpen(true);
+        setSuccessMessage(
+          "کامنت شما با موفقیت ثبت شد پس از تایید توسط ادمین نشان داده خواهد شد",
+        );
+      }
+    } catch (err: any) {
+      console.log("Error in response creat comment :", err);
+      if (err.response?.status === 404) {
+        setIsWarningOpen(true);
+        setWarningMessage(
+          "ماشین مورد نظر برای  کامنت گذاشتن در دیتابیس وجود ندارد لطفا صفحه را رفرش کنید",
+        );
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <div >
-      <form >
-        <p className="text-gray-600 mb-4 text-sm sm:text-base">
-          نشانی ایمیل شما منتشر نخواهد شد
-        </p>
-
-        <div className="mb-4">
-          <label
-            htmlFor="comment"
-            className="block text-sm font-medium text-gray-700 mb-2"
-          >
-            دیدگاه
-          </label>
-          <textarea
-            id="comment"
-            name="comment"
-            rows="6"
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm resize-y"
-            placeholder="نظر خود را اینجا بنویسید..."
-          ></textarea>
-        </div>
-
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-          <div className="mb-4 sm:mb-0">
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700 mb-2"
-            >
-              نام
+    <>
+      <div>
+        <form onSubmit={handleSubmit(onsubmit)}>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              دیدگاه <span className="text-red-600">*</span>
             </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="نام شما"
-            />
+            <textarea
+              className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm resize-y${
+                errors.content ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="نظر خود را اینجا بنویسید..."
+              {...register("content", {
+                minLength: {
+                  value: 2,
+                  message: "کامنت باید حدالقل 2 کاراکتر باشد",
+                },
+                maxLength: {
+                  value: 500,
+                  message: "کامنت حدااکثر 500 کاراکتر می تواند باشد",
+                },
+              })}
+            ></textarea>
+            {errors.content && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.content.message}
+              </p>
+            )}
           </div>
 
           <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700 mb-2"
-            >
-              ایمیل
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              امتیاز
+              <span className="text-gray-500"> (اختیاری)</span>
             </label>
             <input
-              type="email"
-              id="email"
-              name="email"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="test@example.com"
+              type="number"
+              placeholder="امتیاز..."
+              className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300 ${
+                errors.rate ? "border-red-500" : "border-gray-300"
+              }`}
+              {...register("rate", {
+                min: {
+                  value: 1,
+                  message: "حدالقل  1 امتیاز ",
+                },
+                max: {
+                  value: 5,
+                  message: "حداکثر   5 امتیاز ",
+                },
+              })}
             />
+            {errors.rate && (
+              <p className="text-red-500 text-xs mt-1">{errors.rate.message}</p>
+            )}
           </div>
-        </div>
-
-        <div className="mt-6 flex items-start">
-          <input
-            id="remember-me"
-            name="remember-me"
-            type="checkbox"
-            className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-          />
-          <div className="ml-2 flex items-center h-5">
-            <label htmlFor="remember-me" className="text-sm text-gray-600">
-              ذخیره نام، ایمیل در مرورگر برای زمانی که دوباره دیدگاهی می‌نویسم.
-            </label>
+          {replyToId && <input type="hidden" {...register("parent_id")} />}
+          <div className="mt-8 text-center">
+            <button
+              type="submit"
+              className="w-full  px-6 py-3 bg-indigo-500 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 "
+            >
+              فرستادن دیدگاه
+            </button>
           </div>
-        </div>
-
-        <div className="mt-8 text-center">
-          <button
-            type="submit"
-            className="w-full sm:w-auto px-6 py-3 bg-indigo-600 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 "
-          >
-            فرستادن دیدگاه
-          </button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      <SuccessModal
+        isOpen={isSuccessOpen}
+        onClose={() => setIsSuccessOpen(false)}
+        message={successMessage}
+      />
+      <WarningModal
+        isOpen={isWarningOpen}
+        onClose={() => setIsWarningOpen(false)}
+        message={warningMessage}
+      />
+    </>
   );
 };
 
