e7477e51301186ec9b9b17f3d001b271f404934f
niyayesh <niyayeshammardii@gmail.com>
2026-05-29 13:55:46 -0700

    edit name component to function handleShowComment

--- client/src/components/PanelAdmin/ProductForm/ComponentTableProduct.tsx
+++ client/src/components/PanelAdmin/ProductForm/ComponentTableProduct.tsx
@@ -70,7 +70,7 @@ const ComponentTableProduct = () => {
     navigate(`updateImg/${slug}`);
   };
   const handleShowComment = async (id: string) => {
-    navigate(`showComment/${id}`);
+    navigate(`commentoneproduct/${id}`);
   };
 
   return (
