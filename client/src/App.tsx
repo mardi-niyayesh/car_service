7a6cbc34b60e10e99c0c9a21adc85e13314ee572
niyayesh <niyayeshammardii@gmail.com>
2026-05-30 13:33:33 -0700

    change url to rout for component comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -57,8 +57,8 @@ import UpdateCategory from "./components/PanelAdmin/CategoryForm/UpdateCategory"
 import ComponentCreatpoduct from "./components/PanelAdmin/ProductForm/ComponentCreatpoduct";
 import ComponentUpdateProduct from "./components/PanelAdmin/ProductForm/ComponentUpdateProduct";
 import ComponentImgProduct from "./components/PanelAdmin/ProductForm/ComponentImgProduct";
-import CommentOneProduct from "./components/PanelAdmin/CommentForm/CommentOneProduct";
-import ComponentTableComment from "./components/PanelAdmin/CommentForm/ComponentTableComment";
+import CommentOneProduct from "./components/CommentForm/CommentOneProduct";
+import ComponentTableComment from "./components/CommentForm/ComponentTableComment";
 
 function App() {
   return (
