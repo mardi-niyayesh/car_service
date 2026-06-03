0dcee124444b907d3bc9999207455b51a52052a3
homow <homowsh897@gmail.com>
2026-06-03 12:00:48 +0330

    feat(test) add service and mock prisma to test role service

--- server/src/modules/role/role.service.spec.ts
+++ server/src/modules/role/role.service.spec.ts
@@ -1,5 +1,23 @@
-import {describe} from "vitest";
+import type {PrismaMock} from "@/types";
+import {describe, beforeEach, afterEach} from "vitest";
+import {RoleService} from "@/modules/role/role.service";
+import {mockDeep, mockReset} from "vitest-mock-extended";
+import {PrismaService} from "@/modules/prisma/prisma.service";
 
 describe('RoleService', (): void => {
+  let prisma: PrismaMock;
+  let service: RoleService;
 
+  // Start All
+  beforeEach((): void => {
+    prisma = mockDeep<PrismaService>();
+    service = new RoleService(prisma);
+
+    prisma.$transaction.mockImplementation(async (fn) => fn(prisma));
+  });
+
+  // Reset All
+  afterEach((): void => {
+    mockReset(prisma);
+  });
 });
