aff1f8e6bac8844ba30b55cae1851fd27de73ba1
homow <homowsh897@gmail.com>
2026-06-03 23:13:45 +0330

    refactor(test) add new fake permissions

--- server/src/modules/role/role.service.spec.ts
+++ server/src/modules/role/role.service.spec.ts
@@ -5,6 +5,57 @@ import {mockDeep, mockReset} from "vitest-mock-extended";
 import {PrismaService} from "@/modules/prisma/prisma.service";
 import {describe, beforeEach, afterEach, it, expect} from "vitest";
 
+const fakePermissions = [
+  {
+    id: "fd000532-1156-4453-ad75-4a69dec40134",
+    name: "user.view",
+    permission_type: "MANAGER",
+    updated_at: "2026-05-06T22:00:43.687Z",
+    created_at: "2026-05-06T22:00:43.687Z",
+    description: "this permission allows view on user"
+  },
+  {
+    id: "908cce6e-17fe-46f6-8247-a7a4374be5e6",
+    name: "user.delete",
+    permission_type: "MANAGER",
+    updated_at: "2026-05-06T22:00:43.693Z",
+    created_at: "2026-05-06T22:00:43.693Z",
+    description: "this permission allows delete on user"
+  },
+  {
+    id: "b0e23550-d856-41e9-9e61-6e33758b3571",
+    name: "role.assign",
+    permission_type: "MANAGER",
+    updated_at: "2026-05-06T22:00:43.694Z",
+    created_at: "2026-05-06T22:00:43.694Z",
+    description: "this permission allows assign on role"
+  },
+  {
+    id: "81a0b26e-a434-4414-8d79-7c08d521c9de",
+    name: "role.revoke",
+    permission_type: "MANAGER",
+    updated_at: "2026-05-06T22:00:43.695Z",
+    created_at: "2026-05-06T22:00:43.695Z",
+    description: "this permission allows revoke on role"
+  },
+  {
+    id: "b4816a4e-23b1-44f6-a356-e5d38e13e6c6",
+    name: "role.view",
+    permission_type: "MANAGER",
+    updated_at: "2026-05-06T22:00:43.697Z",
+    created_at: "2026-05-06T22:00:43.697Z",
+    description: "this permission allows view on role"
+  },
+  {
+    id: "9440efc2-644b-4bf9-9bde-f1f91cb2b699",
+    name: "permission.view",
+    permission_type: "MANAGER",
+    updated_at: "2026-05-06T22:00:43.699Z",
+    created_at: "2026-05-06T22:00:43.699Z",
+    description: "this permission allows view on permission"
+  }
+] as const;
+
 const fakeRole = {
   id: "6d64a06a-f1a2-44c1-bd47-e557bd57bcc8",
   name: "user_manager",
@@ -13,18 +64,12 @@ const fakeRole = {
   creator_id: null,
   description: "Full administrative access to manage all users in the system",
   role_type: "SYSTEM",
-  rolePermissions: [
-    {
-      permission_id: "fd000532-1156-4453-ad75-4a69dec40134",
-      permission: {
-        name: "user.view",
-        permission_type: "MANAGER",
-        updated_at: "2026-05-06T22:00:43.687Z",
-        created_at: "2026-05-06T22:00:43.687Z",
-        description: "this permission allows view on user"
-      },
-    },
-  ],
+  rolePermissions: fakePermissions.map(p => ({
+    permission_id: p.id,
+    permission: {
+      ...p
+    }
+  })),
 };
 
 describe('RoleService', (): void => {
