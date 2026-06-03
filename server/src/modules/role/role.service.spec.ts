440d615f0f70fd64318f530624af4478866de99e
homow <homowsh897@gmail.com>
2026-06-03 23:08:28 +0330

    feat(test) add new test for find one role

--- server/src/modules/role/role.service.spec.ts
+++ server/src/modules/role/role.service.spec.ts
@@ -1,8 +1,31 @@
 import type {PrismaMock} from "@/types";
-import {describe, beforeEach, afterEach} from "vitest";
 import {RoleService} from "@/modules/role/role.service";
 import {mockDeep, mockReset} from "vitest-mock-extended";
+import {describe, beforeEach, afterEach, it, expect} from "vitest";
 import {PrismaService} from "@/modules/prisma/prisma.service";
+import {Role} from "@/modules/prisma/generated/client";
+
+const fakeRole = {
+  id: "6d64a06a-f1a2-44c1-bd47-e557bd57bcc8",
+  name: "user_manager",
+  updated_at: "2026-05-06T22:00:43.714Z",
+  created_at: "2026-05-06T22:00:43.714Z",
+  creator_id: null,
+  description: "Full administrative access to manage all users in the system",
+  role_type: "SYSTEM",
+  rolePermissions: [
+    {
+      permission_id: "fd000532-1156-4453-ad75-4a69dec40134",
+      permission: {
+        name: "user.view",
+        permission_type: "MANAGER",
+        updated_at: "2026-05-06T22:00:43.687Z",
+        created_at: "2026-05-06T22:00:43.687Z",
+        description: "this permission allows view on user"
+      },
+    },
+  ],
+};
 
 describe('RoleService', (): void => {
   let prisma: PrismaMock;
@@ -19,5 +42,20 @@ describe('RoleService', (): void => {
   // Reset All
   afterEach((): void => {
     mockReset(prisma);
+  });
+
+  /** ================================================
+   * Find One
+   *  ================================================
+   */
+  describe('findOne()', (): void => {
+    // success
+    it('should find role and return with permissions', async () => {
+      prisma.role.findUnique.mockResolvedValue(fakeRole as unknown as Role);
+
+      const {data: {role}} = await service.findOne({name: 'role_manager'});
+
+      expect(role.permissions).toBeDefined();
+    });
   });
 });
