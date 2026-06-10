59c4e1469da16d2db387aa4233ea3bafb14a8dca
homow <homowsh897@gmail.com>
2026-06-10 05:18:05 +0330

    feat(test) config file for test car service

--- server/src/modules/car/car.service.spec.ts
+++ server/src/modules/car/car.service.spec.ts
@@ -1,1 +1,20 @@
-// test car
+import {beforeEach, describe, afterEach} from "vitest";
+import {PrismaMock} from "@/types";
+import {CarService} from "@/modules/car/car.service";
+import {mockDeep, mockReset} from "vitest-mock-extended";
+import {PrismaService} from "@/modules/prisma/prisma.service";
+
+describe('CarService', (): void => {
+  let prisma: PrismaMock;
+  let service: CarService;
+
+  beforeEach((): void => {
+    prisma = mockDeep<PrismaService>();
+    service = new CarService(prisma);
+    prisma.$transaction.mockImplementation(async (fn) => fn(prisma));
+  });
+
+  afterEach((): void => {
+    mockReset(prisma);
+  });
+});
