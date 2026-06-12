30b5680b9d05550caefe64d559c4278df9c68a0a
homow <homowsh897@gmail.com>
2026-06-12 06:29:41 +0330

    feat(test) config category test file

--- server/src/modules/category/category.service.spec.ts
+++ server/src/modules/category/category.service.spec.ts
@@ -1,1 +1,20 @@
-// test category
+import {afterEach, beforeEach, describe} from "vitest";
+import {PrismaMock} from "@/types";
+import {CategoryService} from "./category.service";
+import {mockDeep, mockReset} from "vitest-mock-extended";
+import {PrismaService} from "@/modules/prisma/prisma.service";
+
+describe('CategoryService', () => {
+  let prisma: PrismaMock;
+  let service: CategoryService;
+
+  beforeEach((): void => {
+    prisma = mockDeep<PrismaService>();
+    service = new CategoryService(prisma);
+    prisma.$transaction.mockImplementation(async fn => fn(prisma));
+  });
+
+  afterEach((): void => {
+    mockReset(prisma);
+  });
+});
