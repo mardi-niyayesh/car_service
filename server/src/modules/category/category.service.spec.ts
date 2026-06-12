7c4f1d95f8c2b891b1ad7a4951c35f8f673bd083
homow <homowsh897@gmail.com>
2026-06-12 06:31:16 +0330

    feat(test) add new describe for find one category

--- server/src/modules/category/category.service.spec.ts
+++ server/src/modules/category/category.service.spec.ts
@@ -4,7 +4,7 @@ import {afterEach, beforeEach, describe} from "vitest";
 import {mockDeep, mockReset} from "vitest-mock-extended";
 import {PrismaService} from "@/modules/prisma/prisma.service";
 
-describe('CategoryService', () => {
+describe('CategoryService', (): void => {
   let prisma: PrismaMock;
   let service: CategoryService;
 
@@ -16,5 +16,25 @@ describe('CategoryService', () => {
 
   afterEach((): void => {
     mockReset(prisma);
+  });
+
+  /** ================================================
+   * Find One
+   * ================================================
+   */
+  describe('findOne()', (): void => {
+    const mockCategoryId = 'cat-456';
+    const mockDate = new Date();
+
+    const mockCategory = {
+      id: mockCategoryId,
+      created_at: mockDate,
+      updated_at: mockDate,
+      name: 'SUV',
+      slug: 'suv',
+      description: 'Sport Utility Vehicle category',
+      creator_id: 'user-123',
+    };
+
   });
 });
