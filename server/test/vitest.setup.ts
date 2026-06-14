5de6b8e2f404189c2701a2e6953abbe1900d2a66
homow <homowsh897@gmail.com>
2026-06-14 07:33:24 +0330

    refactor: mock swagger classes(getBaseOkResponseSchema)

--- server/test/vitest.setup.ts
+++ server/test/vitest.setup.ts
@@ -1,6 +1,16 @@
 import 'reflect-metadata';
 import {vi} from "vitest";
 
+vi.mock("@/common", async () => {
+  const actual = await vi.importActual("@/common");
+  return {
+    ...actual,
+    getBaseOkResponseSchema: (_args: unknown) => {
+      return class MockResponse {};
+    },
+  };
+});
+
 vi.mock("@/common/swagger", () => {
   // noinspection JSUnusedGlobalSymbols
   return {
