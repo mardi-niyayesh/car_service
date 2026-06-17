383fb371c7086617e54d9354d52e9236831e5e40
homow <homowsh897@gmail.com>
2026-06-17 17:40:46 +0330

    refactor(decorator) add http status

--- server/src/modules/favorite/decorators/favorite.decorator.ts
+++ server/src/modules/favorite/decorators/favorite.decorator.ts
@@ -1,11 +1,12 @@
 import * as FavoriteDto from "../dto";
 import {ApiOperation} from "@nestjs/swagger";
-import {applyDecorators} from "@nestjs/common";
 import {Permission, PERMISSIONS} from "@/common";
+import {applyDecorators, HttpCode, HttpStatus} from "@nestjs/common";
 
 export const GetListDecorators = () => applyDecorators(
   Permission({
     permissions: [PERMISSIONS.USER_SELF]
   }),
+  HttpCode(HttpStatus.OK),
   ApiOperation(FavoriteDto.favoriteFindAllOperation)
 );
