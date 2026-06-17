4b84003c100d08c9daaa2af09570eb2897e6a7ee
homow <homowsh897@gmail.com>
2026-06-17 09:46:16 +0330

    feat(controller) config favorite controller

--- server/src/modules/favorite/favorite.module.ts
+++ server/src/modules/favorite/favorite.module.ts
@@ -1,4 +1,7 @@
 import {Module} from "@nestjs/common";
+import {FavoriteController} from "./favorite.controller";
 
-@Module({})
+@Module({
+  controllers: [FavoriteController],
+})
 export class FavoriteModule {}
