59f94f30a11fe851c14576532865c057ba91d6fc
homow <homowsh897@gmail.com>
2026-06-17 09:57:45 +0330

    feat(endpoint) add new test endpoint for favorite

--- server/src/modules/favorite/favorite.controller.ts
+++ server/src/modules/favorite/favorite.controller.ts
@@ -1,4 +1,9 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Get} from "@nestjs/common";
 
 @Controller("favorites")
-export class FavoriteController {}
+export class FavoriteController {
+  @Get()
+  get(): string {
+    return "get favorites successfully.";
+  }
+}
