8e5ec88f7e8ab0d659f4ebae947e1cfed6bbc32f
homow <homowsh897@gmail.com>
2026-06-17 16:56:06 +0330

    feat(decorator) add permission to get favorite list

--- server/src/modules/favorite/decorators/favorite.decorator.ts
+++ server/src/modules/favorite/decorators/favorite.decorator.ts
@@ -1,7 +1,11 @@
 import * as FavoriteDto from "../dto";
 import {ApiOperation} from "@nestjs/swagger";
 import {applyDecorators} from "@nestjs/common";
+import {Permission, PERMISSIONS} from "@/common";
 
 export const GetListDecorators = () => applyDecorators(
+  Permission({
+    permissions: [PERMISSIONS.USER_SELF]
+  }),
   ApiOperation(FavoriteDto.favoriteFindAllOperation)
 );
