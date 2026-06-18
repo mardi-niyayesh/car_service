95a5d1b2cb7a584caf9e2ab997662aad67fe880c
homow <homowsh897@gmail.com>
2026-06-18 16:34:33 +0330

    feat(service) create favorite record

--- server/src/modules/favorite/favorite.service.ts
+++ server/src/modules/favorite/favorite.service.ts
@@ -1,8 +1,20 @@
 import {Injectable} from "@nestjs/common";
+import {PrismaService} from "@/modules/prisma/prisma.service";
 
 @Injectable()
 export class FavoriteService {
-  create(user_id: string, car_id: string): string {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(user_id: string, car_id: string) {
+    const favorite = await this.prisma.favorite.create({
+      data: {
+        user_id,
+        car_id
+      }
+    });
+
+    console.log(favorite);
+
     return "favorite create service.";
   }
 }
