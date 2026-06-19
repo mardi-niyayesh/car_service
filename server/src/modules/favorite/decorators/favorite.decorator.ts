5fbccecd9ffe8312d20323965d48e193a134afec
homow <homowsh897@gmail.com>
2026-06-19 19:01:19 +0330

    refactor(style) imports

--- server/src/modules/favorite/decorators/favorite.decorator.ts
+++ server/src/modules/favorite/decorators/favorite.decorator.ts
@@ -1,6 +1,6 @@
 import * as FavoriteDto from "../dto";
-import {PaginationDecoratorQueries, Permission, PERMISSIONS, UUID4Dto} from "@/common";
 import {applyDecorators, HttpCode, HttpStatus} from "@nestjs/common";
+import {PaginationDecoratorQueries, Permission, PERMISSIONS, UUID4Dto} from "@/common";
 import {ApiConflictResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam} from "@nestjs/swagger";
 
 export const GetListDecorators = () => applyDecorators(
