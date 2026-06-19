684879b9af04991f0468df93435560debf0e734c
homow <homowsh897@gmail.com>
2026-06-19 20:10:10 +0330

    feat(type) add new type for favorite list response

--- server/src/types/favorite.types.ts
+++ server/src/types/favorite.types.ts
@@ -1,5 +1,10 @@
+import type {ListWithCount} from "@/types/response.types";
 import type {Favorite} from "@/modules/prisma/generated/client";
 
 export type FavoriteResponse = {
   favorite: Favorite;
 };
+
+export type ListFavoriteResponse = ListWithCount<{
+  favorites: Omit<Favorite, 'user_id'>[];
+}>;
