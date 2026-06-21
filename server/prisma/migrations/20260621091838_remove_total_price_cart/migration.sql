742d05a6d9bb3b896edd057663d8be698fbb2ba4
homow <homowsh897@gmail.com>
2026-06-21 12:49:06 +0330

    refactor(model): delete total_price field from cart model

---
+++ server/prisma/migrations/20260621091838_remove_total_price_cart/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `total_price` on the `carts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "carts" DROP COLUMN "total_price";
