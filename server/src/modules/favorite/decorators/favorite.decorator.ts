2421ab2c05f9a9f031f18f159aba9a092fb0f574
homow <homowsh897@gmail.com>
2026-06-22 18:43:47 +0330

    refactor(style): imports

--- server/src/modules/favorite/decorators/favorite.decorator.ts
+++ server/src/modules/favorite/decorators/favorite.decorator.ts
@@ -3,7 +3,6 @@ import {carSlugSwaggerParam} from "@/modules/car/decorators";
 import {applyDecorators, HttpCode, HttpStatus} from "@nestjs/common";
 import {getUnauthorizedResponse, PaginationDecoratorQueries, Permission, PERMISSIONS, UUID4Dto} from "@/common";
 import {ApiConflictResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiUnauthorizedResponse} from "@nestjs/swagger";
-import {favoriteCheckOperation} from "../dto";
 
 export const GetListDecorators = () => applyDecorators(
   Permission({
