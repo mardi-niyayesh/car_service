40745ad24502424d1663fee15c36714447c73ff2
homow <homowsh897@gmail.com>
2026-06-22 18:33:07 +0330

    feat(decorator): add decorators for check favorite

--- server/src/modules/favorite/decorators/favorite.decorator.ts
+++ server/src/modules/favorite/decorators/favorite.decorator.ts
@@ -2,6 +2,7 @@ import * as FavoriteDto from "../dto";
 import {applyDecorators, HttpCode, HttpStatus} from "@nestjs/common";
 import {getUnauthorizedResponse, PaginationDecoratorQueries, Permission, PERMISSIONS, UUID4Dto} from "@/common";
 import {ApiConflictResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiUnauthorizedResponse} from "@nestjs/swagger";
+import {carSlugSwaggerParam} from "@/modules/car/decorators";
 
 export const GetListDecorators = () => applyDecorators(
   Permission({
@@ -25,4 +26,12 @@ export const CreateDecorator = () => applyDecorators(
   ApiUnauthorizedResponse({type: getUnauthorizedResponse('favorites')}),
   ApiNotFoundResponse({type: FavoriteDto.CreateNotFoundRes}),
   ApiConflictResponse({type: FavoriteDto.CreateConflictRes}),
+);
+
+export const CheckDecorator = () => applyDecorators(
+  Permission({
+    permissions: [PERMISSIONS.USER_SELF],
+  }),
+  HttpCode(HttpStatus.OK),
+  ApiParam(carSlugSwaggerParam),
 );
