5c65bc46bd75fa26d5f5dffeb7346d2431f77b42
homow <homowsh897@gmail.com>
2026-06-25 18:21:53 +0330

    feat(test): config new describe test for create favorite

--- server/src/modules/favorite/favorite.service.spec.ts
+++ server/src/modules/favorite/favorite.service.spec.ts
@@ -3,6 +3,7 @@ import {FavoriteService} from "./favorite.service";
 import {describe, afterEach, beforeEach} from "vitest";
 import {mockDeep, mockReset} from "vitest-mock-extended";
 import {PrismaService} from "@/modules/prisma/prisma.service";
+import {Favorite} from "@/modules/prisma/generated/client";
 
 describe('FavoriteService', (): void => {
   let prisma: PrismaMock;
@@ -15,5 +16,25 @@ describe('FavoriteService', (): void => {
 
   afterEach((): void => {
     mockReset(prisma);
+  });
+
+  const mockDate = new Date();
+  const mockUserId = 'user-123';
+  const mockCarId = 'car-789';
+
+  const mockFavorite: Favorite = {
+    id: 'fav-456',
+    created_at: mockDate,
+    updated_at: mockDate,
+    user_id: mockUserId,
+    car_id: mockCarId,
+  };
+
+  /** ================================================
+   * Create (Add to favorites)
+   * ================================================
+   */
+  describe('create()', (): void => {
+
   });
 });
