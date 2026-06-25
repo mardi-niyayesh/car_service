fb3bf3fa10198aca38164a0851040dd611016b49
homow <homowsh897@gmail.com>
2026-06-25 18:16:18 +0330

    refactor: import styles

--- server/src/modules/favorite/favorite.service.spec.ts
+++ server/src/modules/favorite/favorite.service.spec.ts
@@ -1,7 +1,7 @@
-import {mockDeep, mockReset} from "vitest-mock-extended";
-import {describe, afterEach, beforeEach} from "vitest";
-import {PrismaMock} from "@/types";
+import type {PrismaMock} from "@/types";
 import {FavoriteService} from "./favorite.service";
+import {describe, afterEach, beforeEach} from "vitest";
+import {mockDeep, mockReset} from "vitest-mock-extended";
 import {PrismaService} from "@/modules/prisma/prisma.service";
 
 describe('FavoriteService', (): void => {
@@ -11,7 +11,6 @@ describe('FavoriteService', (): void => {
   beforeEach((): void => {
     prisma = mockDeep<PrismaService>();
     service = new FavoriteService(prisma);
-    prisma.$transaction.mockImplementation(async (fn) => fn(prisma));
   });
 
   afterEach((): void => {
