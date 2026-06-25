c6f0450c770926e225bf3cce2789e652f12989f9
homow <homowsh897@gmail.com>
2026-06-25 18:13:55 +0330

    chore(test): config test favorite

--- server/src/modules/favorite/favorite.service.spec.ts
+++ server/src/modules/favorite/favorite.service.spec.ts
@@ -1,1 +1,20 @@
-// test
+import {mockDeep, mockReset} from "vitest-mock-extended";
+import {describe, afterEach, beforeEach} from "vitest";
+import {PrismaMock} from "@/types";
+import {FavoriteService} from "./favorite.service";
+import {PrismaService} from "@/modules/prisma/prisma.service";
+
+describe('FavoriteService', (): void => {
+  let prisma: PrismaMock;
+  let service: FavoriteService;
+
+  beforeEach((): void => {
+    prisma = mockDeep<PrismaService>();
+    service = new FavoriteService(prisma);
+    prisma.$transaction.mockImplementation(async (fn) => fn(prisma));
+  });
+
+  afterEach((): void => {
+    mockReset(prisma);
+  });
+});
