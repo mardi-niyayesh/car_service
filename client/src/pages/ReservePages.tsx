8691afb3aaca612ad918a3173564bfa82a4daaf3
niyayesh <niyayeshammardii@gmail.com>
2026-06-26 11:08:50 -0700

    edit ui

--- client/src/pages/ReservePages.tsx
+++ client/src/pages/ReservePages.tsx
@@ -4,15 +4,14 @@ const ReservePages = () => {
     <>
       <div className="flex flex-col md:flex-row gap-3 md:gap-4 p-3 md:p-4 min-h-screen bg-white">
         <div className="w-full md:flex-1">
-          <div className=" border border-[#EDEDED] rounded-xl bg-blue-100 shadow-sm sm:gap-0 p-3 sm:p-4  ">
-            <p className="text-blue-800 text-[22px] font-bold  text-center">
+          <div className=" border border-[#EDEDED] rounded-xl bg-yellow-100 shadow-sm sm:gap-0 p-3 sm:p-4  ">
+            <p className="text-yellow-800 text-[22px] font-bold  text-center">
               رزروهای جاری من
             </p>
           </div>
-            <ResentReseve />
+          <ResentReseve />
         </div>
       </div>
-    
     </>
   );
 };
