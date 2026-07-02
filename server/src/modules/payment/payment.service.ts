240708e7fca8f7ec31639181b66bcee676652323
homow <homowsh897@gmail.com>
2026-07-02 08:41:54 +0330

    feat(service): check car rent in service

--- server/src/modules/payment/payment.service.ts
+++ server/src/modules/payment/payment.service.ts
@@ -1,11 +1,24 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, NotFoundException} from "@nestjs/common";
 import {PrismaService} from "@/modules/prisma/prisma.service";
+import {BaseException} from "@/types";
 
 @Injectable()
 export class PaymentService {
   constructor(private readonly prisma: PrismaService) {}
 
-  payment(user_id: string, payment_id: string) {
+  async payment(user_id: string, car_rent_id: string) {
+    const payment = await this.prisma.carRent.findUnique({
+      where: {
+        id: car_rent_id,
+        cart: {
+          user_id
+        }
+      }
+    });
 
+    if (!payment) throw new NotFoundException({
+      message: "Car rent not found in database, please try again later.",
+      error: "Car Rent Not Found"
+    } as BaseException);
   }
 }
