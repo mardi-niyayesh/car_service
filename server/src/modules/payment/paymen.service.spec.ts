91629139f5cb47e97073cc274704b0ac1d936809
homow <homowsh897@gmail.com>
2026-07-14 08:13:31 +0330

    feat(test): config a new describe for payment

--- server/src/modules/payment/paymen.service.spec.ts
+++ server/src/modules/payment/paymen.service.spec.ts
@@ -4,6 +4,7 @@ import {describe, beforeEach, afterEach} from "vitest";
 import {RedisService} from "@/modules/redis/redis.service";
 import {PrismaService} from "@/modules/prisma/prisma.service";
 import {type DeepMockProxy, mockDeep, mockReset} from "vitest-mock-extended";
+import {CarRent, Payment, PaymentStatus, RentStatus} from "@/modules/prisma/generated/client";
 
 describe('PaymentService', (): void => {
   let prisma: PrismaMock;
@@ -22,5 +23,41 @@ describe('PaymentService', (): void => {
   afterEach((): void => {
     mockReset(redis);
     mockReset(prisma);
+  });
+
+  const mockDate = new Date();
+  const mockUserId = 'user-123';
+  const mockCarRentId = 'rent-789';
+  const mockPaymentId = 'pay-456';
+
+  const mockCarRent: CarRent = {
+    id: mockCarRentId,
+    price: 400000,
+    description: 'Need car for business trip',
+    car_id: 'car-789',
+    end_date: new Date('2024-12-22'),
+    start_date: new Date('2024-12-20'),
+    cart_id: 'cart-456',
+    status: RentStatus.PENDING,
+    created_at: mockDate,
+    updated_at: mockDate,
+  };
+
+  const mockPayment: Payment = {
+    id: mockPaymentId,
+    status: PaymentStatus.SUCCESS,
+    amount: 400000,
+    transaction_id: 'TXN-1234567890-abc123',
+    car_rent_id: mockCarRentId,
+    created_at: mockDate,
+    updated_at: mockDate,
+  };
+
+  /** ================================================
+   * Payment
+   * ================================================
+   */
+  describe('payment()', (): void => {
+
   });
 });
