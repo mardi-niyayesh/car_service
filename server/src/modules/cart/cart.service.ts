7e7fd7a11cd2d1b2c91d893149cc5040813fc0b5
homow <homowsh897@gmail.com>
2026-07-18 17:48:24 +0330

    refactor(endpoint): add car rent status query to get cart

--- server/src/modules/cart/cart.service.ts
+++ server/src/modules/cart/cart.service.ts
@@ -1,8 +1,8 @@
 import * as CartDto from "./dto";
+import {eventsEmitter} from "@/common";
 import {OnEvent} from "@nestjs/event-emitter";
 import {RentStatus} from "@/modules/prisma/generated/enums";
 import {PrismaService} from "@/modules/prisma/prisma.service";
-import {eventsEmitter, PaginationValidatorType} from "@/common";
 import {ConflictException, Injectable, NotFoundException} from '@nestjs/common';
 import type {CarRentWhereInput} from "@/modules/prisma/generated/models/CarRent";
 import type {ApiResponse, BaseException, CartResponse, CreateCartSignup, UserAccess, CarRentResponse, RemoveCarRentResponse} from "@/types";
@@ -26,11 +26,11 @@ export class CartService {
   /** get self cart
    * - **only roles with permission (user.self) can accessibility to this route**
    */
-  async getCart(user_id: string, user: UserAccess, paginate: PaginationValidatorType): Promise<ApiResponse<CartResponse>> {
+  async getCart(user_id: string, user: UserAccess, paginate: CartDto.GetCartValidatorType): Promise<ApiResponse<CartResponse>> {
     const {limit, offset, orderByLower} = paginate;
 
     const whereCarRent: CarRentWhereInput = {
-      status: RentStatus.PENDING
+      status: paginate.rent_status
     };
 
     const cart = await this.prisma.cart.findUnique({
